55caf1f03a474b13f649c2bca3d15e7f2d998312
Jason Skomorowski <jason@indexdata.com>
2016-12-08 16:13:55 -0500

    Use ECMAscript stage-2 rather than stage-0
    We should perhaps narrow this even further but at least it's not
    anything-goes anymore.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -38,7 +38,7 @@ module.exports = {
         query: {
           presets: [
               require.resolve("babel-preset-es2015"),
-              require.resolve("babel-preset-stage-0"),
+              require.resolve("babel-preset-stage-2"),
               require.resolve("babel-preset-react")
           ]
         },
