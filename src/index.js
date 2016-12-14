09f4ab3851a6a26697d5198cd2fa071aa3201978
Jason Skomorowski <jason@indexdata.com>
2016-12-14 17:28:06 -0500

    Put Okapi metadata in store for stripes-connect STRIPES-11

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import configureStore from './configureStore';
 import Root from './Root';
+import { okapi } from 'stripes-loader!';
 
 // global styles need an explicit loader to be exempt from the 
 // local-scoping of the default loader configuration
 //import '!style!css!postcss!../styles/global.css';
 
-const store = configureStore();
+const initialState = { okapi };
+const store = configureStore(initialState);
 
 render(
   <Root store={store} />,
--- src/initialReducers.js
+++ src/initialReducers.js
@@ -1,5 +1,7 @@
 import { reducer as form } from 'redux-form';
 
 export default {
+  // TODO: here's where you'd pull in a reducer to handle Okapi actions like auth
+  okapi: (state = {}) => state,
   form: form,
 };
