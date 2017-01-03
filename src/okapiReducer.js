fbadf08457cefd51507b5f81beb24a863178117e
Mike Taylor <mike@indexdata.com>
2017-01-03 11:53:45 +0000

    Resolve simple lint errors in okapiReducer (spacing, trailing commas)

--- src/okapiReducer.js
+++ src/okapiReducer.js
@@ -2,13 +2,13 @@
 
 export default function okapiReducer(state = {}, action) {
   switch (action.type) {
-      case 'SET_TOKEN':
-        return Object.assign({}, state, {
-          token: action.token
-        });
-      case 'CLEAR_TOKEN': 
-        return Object.assign({}, state, token: null);
-      default:
-        return state;
+    case 'SET_TOKEN':
+      return Object.assign({}, state, {
+        token: action.token,
+      });
+    case 'CLEAR_TOKEN':
+      return Object.assign({}, state, token: null);
+    default:
+      return state;
   }
 }
