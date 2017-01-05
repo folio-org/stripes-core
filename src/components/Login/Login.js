eed1db26fd19d2229641265d3e681e3ef255db7d
JCoburn <JCoburn@CORP-IT-250WM6.ebsco.com>
2017-01-05 14:46:04 -0600

    Added button and loading indicator to Login form. Updated styling.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,30 +2,32 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import css from './Login.css';
 
-function Login() {
-  return (
-    <div className={css.loginOverlay}>
-      <div className={css.loginContainer}>
-        <h1>Sign In</h1>
-        <form>
-          <div className={css.slabStack}>
-            <div className={css.majorSlab}>
-              <Field className={css.loginInput} name="username" type="text" component="input" placeholder="Username" />
+function Login(){
+  return(
+      <div className={css.loginOverlay}>
+        <div className={css.loginContainer}>
+          <h1>Sign In</h1>
+          <form>
+            <div className={css.slabStack}>
+              <div className={css.majorSlab}>
+                <Field className={css.loginInput} name="username" type="text" component="input" placeholder="Username" />
+              </div>
+              <div className={css.minorSlab}>
+                <Field className={css.loginInput} name="password" type="password" component="input" placeholder="Password" />
+              </div>
+              <button type="button" className={css.slabButton}>
+              Log in
+              </button>
+              {/*<div className={css.loading +" "+css.blue}></div>
+              <div>
+                <span className={css.loginError} >Sorry, the information entered does not match our records!</span>
+              </div> 
+              */}
+              
             </div>
-            <div className={css.minorSlab}>
-              <Field className={css.loginInput} name="password" type="password" component="input" placeholder="Password" />
-            </div>
-            {/* This portion for login validation...
-            <div>
-              <span className={css.loginError} >
-                Sorry, information entered does not match our records!
-              </span>
-            </div>
-            */}
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
   );
 }
 
