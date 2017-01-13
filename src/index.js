8ee1953c55397af826e02b045a6016acad1a8192
Mike Taylor <mike@indexdata.com>
2017-01-13 15:44:35 +0000

    Pass "config", as well as "okapi", through from stripes-loader's export to configureStore().

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import configureStore from './configureStore';
 import Root from './Root';
-import { okapi } from 'stripes-loader!'; // eslint-disable-line
+import { okapi, config } from 'stripes-loader!'; // eslint-disable-line
 
 const initialState = { okapi };
-const store = configureStore(initialState);
+const store = configureStore(initialState, config);
 
 render(
   <Root store={store} />,
