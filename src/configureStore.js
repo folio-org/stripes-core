03e71d6fe91f295fc78c240f8a408f9ad3e21301
Mike Taylor <mike@indexdata.com>
2017-01-13 11:05:27 +0000

    Add facility to disable redux-action logging by setting the global window.reduxLog to false. Fixes STRIPES-137.

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,10 +3,19 @@ import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import initialReducers from './initialReducers';
 
+
+const logger = createLogger({
+  predicate: function(action) {
+    const show = window.reduxLog;
+    // Show loggig, unless explicitly set false
+    return show === undefined || show;
+  },
+});
+
 export default function configureStore(initialState) {
   const finalCreateStore = compose(
     applyMiddleware(thunk),
-    applyMiddleware(createLogger()),
+    applyMiddleware(logger),
   )(createStore);
 
   const reducer = combineReducers(initialReducers);
