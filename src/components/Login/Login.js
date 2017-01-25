3eafaf49b70554f681f2a108c75f83a426b16841
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-01-25 20:58:51 +0100

    Clears lint errors in Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,38 +9,38 @@ const propTypes = {
 };
 
 function Login(props) {
-
   const {
     handleSubmit,
     pristine,
     submitting,
   } = props;
 
-  return(
-      <div className={css.loginOverlay}>
-        <div className={css.loginContainer}>
-          <h1>Sign In</h1>
-          <form>
-            <div className={css.slabStack}>
-              <div className={css.majorSlab}>
-                <Field className={css.loginInput} name="username" type="text" component="input" placeholder="Username" />
-              </div>
-              <div className={css.minorSlab}>
-                <Field className={css.loginInput} name="password" type="password" component="input" placeholder="Password" />
-              </div>
-              <button type="submit" className={css.slabButton} onClick={handleSubmit} disabled={submitting||pristine}>
-              Log in
-              </button>
-              {/*<div className={css.loading +" "+css.blue}></div>
-              <div>
-                <span className={css.loginError} >Sorry, the information entered does not match our records!</span>
-              </div> 
-              */}
-              
+  return (
+    <div className={css.loginOverlay}>
+      <div className={css.loginContainer}>
+        <h1>Sign In</h1>
+        <form>
+          <div className={css.slabStack}>
+            <div className={css.majorSlab}>
+              <Field className={css.loginInput} name="username" type="text" component="input" placeholder="Username" />
             </div>
-          </form>
-        </div>
+            <div className={css.minorSlab}>
+              <Field className={css.loginInput} name="password" type="password" component="input" placeholder="Password" />
+            </div>
+            <button type="submit" className={css.slabButton} onClick={handleSubmit} disabled={submitting || pristine}>
+            Log in
+            </button>
+            {/* <div className={css.loading +" "+css.blue}></div>
+            <div>
+              <span className={css.loginError} >
+                Sorry, the information entered does not match our records!
+              </span>
+            </div>
+            */}
+          </div>
+        </form>
       </div>
+    </div>
   );
 }
 
