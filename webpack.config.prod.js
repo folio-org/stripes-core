874277bdb17f07c003fb2f6fdcb78595f6e85aa3
Jason Skomorowski <jason@indexdata.com>
2017-02-02 16:58:33 -0500

    Disable failing minification STRIPES-102

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,11 +13,11 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compressor: {
+    //     warnings: false
+    //   }
+    // }),
     new ExtractTextPlugin("global.css", {
       allChunks: true
     }),
