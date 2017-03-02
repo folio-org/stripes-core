c8a513010a2db6ffb3040c0be553dbbf4b56ac24
Jason Skomorowski <jason@indexdata.com>
2017-03-02 17:08:10 -0500

    Use a specific copy of react when stripes-core is linked STRIPES-220

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -4,13 +4,24 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+// React doesn't like being included multiple times as can happen when using
+// yarn link. Here we find a more specific path to it by first looking in
+// stripes-core (__dirname) before falling back to the platform or simply react
+let specificReact = path.join(__dirname, 'node_modules', 'react');
+try {
+  require.resolve(specificReact);
+} catch (e) {
+  specificReact = 'react';
+}
+
 module.exports = {
   entry: [
     path.join(__dirname, 'src', 'index')
   ],
   resolve: {
     alias: {
-      'stripes-loader': '@folio/stripes-loader'
+      'stripes-loader': '@folio/stripes-loader',
+      'react': specificReact,
     }
   },
   module: {
