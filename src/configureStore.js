c9ca74f4ddb4db28ce6467b93cd5efb1189828e6
Mike Taylor <mike@indexdata.com>
2017-03-02 21:03:52 +0000

    Simplify configuration of Redux logging. Now log when and only when the main logger object has the 'redux' category. Old Stripes configurations that had 'reduxLog' set true or false should replace this with the corrsponding presence or absence of 'redux' among the categoties listed in the 'logCategories' setting.

--- src/configureStore.js
+++ src/configureStore.js
@@ -6,13 +6,7 @@ import initialReducers from './initialReducers';
 export default function configureStore(initialState, config, stripesLogger) {
   const logger = createLogger({
     // Show logging unless explicitly set false
-    predicate: () => {
-      // XXX should change to check stripesLogger->hasCategory('redux')
-      let res = window.reduxLog;
-      if (res === undefined && config) res = config.reduxLog;
-      if (res !== undefined) return res;
-      return true; // Default default if neither global variable nor config item is set.
-    },
+    predicate: () => stripesLogger.hasCategory('redux'),
   });
 
   const finalCreateStore = compose(
