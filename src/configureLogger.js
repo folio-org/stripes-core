6bbc1cd27a7bc21b7ec2e5c245032aecd31389b0
Mike Taylor <mike@indexdata.com>
2017-03-03 17:59:47 +0000

    Use lodash.get to obtain logger configuration, so nothing goes wrong if there is no 'config' section in stripes.config.js. Also, make the default set of categories just 'redux', to match the old behaviour.

--- src/configureLogger.js
+++ src/configureLogger.js
@@ -1,9 +1,10 @@
+import _ from 'lodash';
 import Logger from '@folio/stripes-logger';
 
 export default function configureLogger(config) {
-  const categories = config.logCategories || 'path';
-  const prefix = config.logPrefix || 'stripes';
-  const timestamp = config.logTimestamp || false;
+  const categories = _.get(config, ['logCategories'], 'redux');
+  const prefix = _.get(config, ['logPrefix'], 'stripes');
+  const timestamp = _.get(config, ['logTimestamp'], false);
 
   return new Logger(categories, prefix, timestamp);
 }
