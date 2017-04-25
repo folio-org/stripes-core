folio-org/stripes-core

commit 36fba133efe6a44c7590e140c3691bf023eaa644

Fix logout
  Ensure all state is cleared by re-loading app

src/components/MainNav/MainNav.js
@@ -53,9 +53,7 @@ class MainNav extends Component {
   }
 
   logout() {
-    this.store.dispatch(clearOkapiToken());
-    this.store.dispatch(clearCurrentUser());
-    this.context.router.history.push('/');
+    window.location.assign('/');
   }
 
   render() {
@@ -71,7 +69,7 @@ class MainNav extends Component {
         <path d="M13 19.5c-3.4 0-6.1-3.6-6.1-8.1 0-3.5 0.6-8.1 6.1-8.1 5.5 0 6.1 4.6 6.1 8.1C19.1 15.9 16.4 19.5 13 19.5zM13 4.3c-3.6 0-5.1 2.1-5.1 7.1 0 3.9 2.3 7.1 5.1 7.1 2.8 0 5.1-3.2 5.1-7.1C18.1 6.5 16.6 4.3 13 4.3z" style={{ fill: '#3D9964' }} />
       </svg>
     );
-
+window.location.assign('/');
     let maybePerms;
     const config = stripes.config;
     if (config && config.showPerms) {

commit 289dbfd4ddf7fae95f4af6e495f90d37ae34cb97

If config.softLogout is defined, perform a soft logout
This allows you to set up your stripes.config.js to undo the damage
caused by commit 36fba133efe6a44c7590e140c3691bf023eaa644 -- which in
turn enables me to do the testing for the last part of STRIPES-326
with reasonable efficiency.

src/components/MainNav/MainNav.js
@@ -53,7 +53,14 @@ class MainNav extends Component {
   }
 
   logout() {
-    window.location.assign('/');
+    const config = this.props.stripes.config;
+    if (config && config.softLogout) {
+      this.store.dispatch(clearOkapiToken());
+      this.store.dispatch(clearCurrentUser());
+      this.context.router.history.push('/');
+    } else {
+      window.location.assign('/');
+    }
   }
 
   render() {