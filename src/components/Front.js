ff84e35ce7b3251e8cce4e877c8fe4356f807eea
Mike Taylor <mike@indexdata.com>
2017-05-08 16:39:29 +0100

    Add <Link> to /about. Part of STRIPES-356

--- src/components/Front.js
+++ src/components/Front.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Link from 'react-router-dom/Link';
 
-export const Front = () => <h3>Welcome to whatever this is!</h3>;
+export const Front = () => (
+  <div>
+    <h3>Welcome to whatever this is!</h3>
+    <p><Link to="/about">About</Link></p>
+  </div>
+);
 export default Front;
