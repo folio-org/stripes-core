dd6fa97295636e599b8ad70522de42391abab00b
Mike Taylor <mike@indexdata.com>
2017-05-12 17:23:51 +0100

    eslint-disable comment

--- src/mainActions.js
+++ src/mainActions.js
@@ -4,4 +4,8 @@ function resetStore() {
   };
 }
 
+// We export a single named function rather than using a default
+// export, to remain consistent with okapiActions.js
+//
+// eslint-disable-next-line import/prefer-default-export
 export { resetStore };
