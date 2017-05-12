5d0e24795c033da5ab5e4bc6b8b35cc854af6e8d
Mike Taylor <mike@indexdata.com>
2017-05-12 16:13:48 +0100

    New file enhanceReducer.js
    Promotes the reducer-enhancement of configureStore to its own
    function, so it can be re-used.
    
    Part of STRIPES-333.

---
+++ src/enhanceReducer.js
@@ -0,0 +1,17 @@
+// Returns a reducer function equivalent to the one passed in, except
+// that it additional implements the 'RESET_STORE' action, which
+// removes everything except the Okapi object from the store.
+
+function enhanceReducer(reducer) {
+  return (state, action) => {
+    console.log(`enhanced reducer considering action ${action.type}`);
+    if (action.type === 'RESET_STORE') {
+      console.log('interpreting RESET_STORE');
+      return { okapi: state.okapi };
+    } else {
+      return reducer(state, action);
+    }
+  };
+}
+
+export default enhanceReducer;
