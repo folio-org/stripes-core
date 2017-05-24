1d47869707419c356c70c84075e9a3856a201efc
Mike Taylor <mike@indexdata.com>
2017-05-24 01:29:17 +0100

    Use AddContext to put stripes object on context for apps and settings
    The code is a bit too similar in those two places (moduleRoutes.js and
    components/Settings/Settings.js) -- we should think about whether
    there is a way to have them share their core.
    
    Fixes STRIPES-388.

--- src/moduleRoutes.js
+++ src/moduleRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Route from 'react-router-dom/Route';
 import { connectFor } from '@folio/stripes-connect';
 import { modules } from 'stripes-loader'; // eslint-disable-line
+import AddContext from './AddContext';
 
 if (!Array.isArray(modules.app) && modules.length < 0) {
   throw new Error('At least one module of type "app" must be enabled.');
@@ -15,12 +16,17 @@ function getModuleRoutes(stripes) {
 
     const connect = connectFor(module.module, stripes.logger);
     const Current = connect(module.getModule());
+    const moduleStripes = Object.assign({}, stripes, { connect });
 
     return (
       <Route
         path={module.route}
         key={module.route}
-        render={props => <Current {...props} connect={connect} stripes={Object.assign({}, stripes, { connect })} />}
+        render={props =>
+          <AddContext context={{ stripes: moduleStripes }}>
+            <Current {...props} connect={connect} stripes={moduleStripes} />
+          </AddContext>
+        }
       />
     );
   }).filter(x => x);
