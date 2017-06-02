b1181979a1696b9fda2cbf2a0c71a7e521966244
Mike Taylor <mike@indexdata.com>
2017-06-02 16:08:51 +0100

    Revert "<MainContainer> listens to all route changes. Towards STRIPES-362"
    This reverts commit b3c52386170f0dc8d59838903e3456ef08dac75e.
    
    (Because we want to listen in MainNav.js instead.)

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,30 +1,19 @@
 import React from 'react';
+// import globalCssReset from '!style!css!normalize.css';
 import globalSystemCss from '!style-loader!css-loader!./global.css'; // eslint-disable-line
-import { withRouter } from 'react-router';
 
 import css from './MainContainer.css';
 
-class MainContainer extends React.Component {
-  static propTypes = {
-    children: React.PropTypes.node.isRequired,
-    history: React.PropTypes.shape({
-      listen: React.PropTypes.func.isRequired,
-    }).isRequired,
-  };
+const propTypes = {
+  children: React.PropTypes.node.isRequired,
+};
 
-  constructor(props) {
-    super(props);
-    props.history.listen((hist, op) => {
-      // eslint-disable-next-line no-console
-      console.log('MainContainer history changed: hist =', hist, 'op =', op);
-    });
-  }
-
-  render() {
-    return (
-      <div className={css.root}>{this.props.children}</div>
-    );
-  }
+function MainContainer(props) {
+  return (
+    <div className={css.root}>{props.children}</div>
+  );
 }
 
-export default withRouter(MainContainer);
+MainContainer.propTypes = propTypes;
+
+export default MainContainer;
