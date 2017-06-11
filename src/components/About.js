dcfb1791d1715d0660ed6635f153295561017825
Mike Taylor <mike@indexdata.com>
2017-06-11 18:16:33 +0100

    About page shows back-end modules and interfaces
    Fixes STRIPES-410.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { modules } from 'stripes-loader'; // eslint-disable-line
+import _ from 'lodash';
+import React, { PropTypes } from 'react';
+import { modules as uiModules } from 'stripes-loader'; // eslint-disable-line
 
 import stripesCore from '@folio/stripes-core/package.json'; // eslint-disable-line
 import stripesLoader from '@folio/stripes-loader/package.json';
@@ -10,7 +11,7 @@ import stripesLogger from '@folio/stripes-logger/package.json';
 import Pane from '@folio/stripes-components/lib/Pane';
 import Paneset from '@folio/stripes-components/lib/Paneset';
 
-const About = () => {
+const About = (props) => {
   function listModules(caption, list) {
     const n = list.length;
     return (
@@ -25,6 +26,11 @@ const About = () => {
     );
   }
 
+  const modules = _.get(props.stripes, ['discovery', 'modules']) || {};
+  const interfaces = _.get(props.stripes, ['discovery', 'interfaces']) || {};
+  const nm = Object.keys(modules).length;
+  const ni = Object.keys(interfaces).length;
+
   return (
     <Paneset>
       <Pane defaultWidth="50%" paneTitle="User interface">
@@ -36,13 +42,30 @@ const About = () => {
           <li key="stripes-components">stripes-components {stripesComponents.version}</li>
           <li key="stripes-logger">stripes-logger {stripesLogger.version}</li>
         </ul>
-        {Object.keys(modules).map(key => listModules(key, modules[key]))}
+        {Object.keys(uiModules).map(key => listModules(key, uiModules[key]))}
       </Pane>
       <Pane defaultWidth="50%" paneTitle="Okapi services">
-        xxx
+        <h4>{nm} module{nm === 1 ? '' : 's'}</h4>
+        <ul>
+          {Object.keys(modules).sort().map(key => <li key={key}>{modules[key]} (<tt>{key}</tt>)</li>)}
+        </ul>
+
+        <h4>{ni} interface{ni === 1 ? '' : 's'}</h4>
+        <ul>
+          {Object.keys(interfaces).sort().map(key => <li key={key}>{key} v{interfaces[key]}</li>)}
+        </ul>
       </Pane>
     </Paneset>
   );
+};
+
+About.propTypes = {
+  stripes: PropTypes.shape({
+    discovery: PropTypes.shape({
+      modules: PropTypes.object,
+      interfaces: PropTypes.object,
+    }),
+  }).isRequired,
 };
 
 export default About;
