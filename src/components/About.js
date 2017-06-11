cfe78d22d839ac76ec837b84c8d5f6003173eb24
Mike Taylor <mike@indexdata.com>
2017-06-11 17:46:04 +0100

    Change About page to to-pane version
    Okapi-services pane is empty at this stage.
    
    Towards STRIPES-400.

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,9 @@ import stripesConnect from '@folio/stripes-connect/package.json';
 import stripesComponents from '@folio/stripes-components/package.json';
 import stripesLogger from '@folio/stripes-logger/package.json';
 
+import Pane from '@folio/stripes-components/lib/Pane';
+import Paneset from '@folio/stripes-components/lib/Paneset';
+
 const About = () => {
   function listModules(caption, list) {
     const n = list.length;
@@ -23,18 +26,22 @@ const About = () => {
   }
 
   return (
-    <div>
-      <h3>About Stripes</h3>
-      <h4>Foundation</h4>
-      <ul>
-        <li key="stripes-core">stripes-core {stripesCore.version}</li>
-        <li key="stripes-loader">stripes-loader {stripesLoader.version}</li>
-        <li key="stripes-connect">stripes-connect {stripesConnect.version}</li>
-        <li key="stripes-components">stripes-components {stripesComponents.version}</li>
-        <li key="stripes-logger">stripes-logger {stripesLogger.version}</li>
-      </ul>
-      {Object.keys(modules).map(key => listModules(key, modules[key]))}
-    </div>
+    <Paneset>
+      <Pane defaultWidth="50%" paneTitle="User interface">
+        <h4>Foundation</h4>
+        <ul>
+          <li key="stripes-core">stripes-core {stripesCore.version}</li>
+          <li key="stripes-loader">stripes-loader {stripesLoader.version}</li>
+          <li key="stripes-connect">stripes-connect {stripesConnect.version}</li>
+          <li key="stripes-components">stripes-components {stripesComponents.version}</li>
+          <li key="stripes-logger">stripes-logger {stripesLogger.version}</li>
+        </ul>
+        {Object.keys(modules).map(key => listModules(key, modules[key]))}
+      </Pane>
+      <Pane defaultWidth="50%" paneTitle="Okapi services">
+        xxx
+      </Pane>
+    </Paneset>
   );
 };
 
