55130234fac4eaf99b4679b6e88d0dd0ba161a95
Jason Skomorowski <jason@indexdata.com>
2017-06-15 14:22:16 -0400

    Enable babel's caching
    Let me know if this is anything other than helpful

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -38,7 +38,8 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        query: {
+        options: {
+          cacheDirectory: true,
           presets: [
               require.resolve("babel-preset-es2015"),
               require.resolve("babel-preset-stage-2"),
