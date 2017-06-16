743b34469b870f47acc97973accb9f7b92c8e3cb
Mike Taylor <mike@indexdata.com>
2017-06-16 14:22:03 +0100

    Include keyBindingsReducer is initial reducers.
    Part of STRPCORE-2.

--- src/initialReducers.js
+++ src/initialReducers.js
@@ -1,10 +1,12 @@
 import { reducer as form } from 'redux-form';
 import okapiReducer from './okapiReducer';
 import { discoveryReducer } from './discoverServices';
+import { keyBindingsReducer } from './discoverKeyBindings';
 
 export default {
   // TODO: here's where you'd pull in a reducer to handle Okapi actions like auth
   okapi: okapiReducer,
   discovery: discoveryReducer,
+  bindings: keyBindingsReducer,
   form,
 };
