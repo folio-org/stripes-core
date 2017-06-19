2822b5653ad02b88416f585f2a9f8605b2cab30f
Mike Taylor <mike@indexdata.com>
2017-06-19 13:25:23 +0100

    Discovery information is retained after logout
    Allows the "About" page to continue working after a logout and re-login.
    
    Fixes STRPCORE-4.

--- src/enhanceReducer.js
+++ src/enhanceReducer.js
@@ -1,10 +1,16 @@
 // Returns a reducer function equivalent to the one passed in, except
 // that it additionally implements the 'RESET_STORE' action, which
-// removes everything except the Okapi object from the store.
+// removes everything except the Okapi object and discovery
+// information from the store.
 
 function enhanceReducer(reducer) {
   return (state, action) =>
-    ((action.type === 'RESET_STORE') ? { okapi: state.okapi } : reducer(state, action));
+    (action.type !== 'RESET_STORE' ? reducer(state, action) :
+    {
+      okapi: state.okapi,
+      discovery: state.discovery,
+    }
+  );
 }
 
 export default enhanceReducer;
