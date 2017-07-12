32ec9be9402658485eba0e55803db91086214326
Michal Kuklis <michal.kuklis@gmail.com>
2017-07-12 16:18:14 -0400

    Cleanup

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import sideEffects from '@folio/stripes-connect';
