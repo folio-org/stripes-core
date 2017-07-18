22d2670df0578061e053a80518e23cc346bafec9
Mike Taylor <mike@indexdata.com>
2017-07-18 19:20:00 +0100

    Include actions from stripes-components as well as modules
    Another part of STCOR-36.

--- src/gatherActions.js
+++ src/gatherActions.js
@@ -1,6 +1,15 @@
 // Gather actionNames from all registered modules for hot-key mapping
 
 import { modules } from 'stripes-loader'; // eslint-disable-line
+import stripesComponents from '@folio/stripes-components/package.json';
+
+function addKeys(register, list) {
+  if (list) {
+    for (const actionName of list) {
+      register[actionName] = true;
+    }
+  }
+}
 
 export default function gatherActions() {
   const allActions = {};
@@ -9,14 +18,11 @@ export default function gatherActions() {
     const set = modules[key];
     for (const key2 of Object.keys(set)) {
       const module = set[key2];
-      const keymap = module.actionNames;
-      if (keymap) {
-        for (const actionName of keymap) {
-          allActions[actionName] = true;
-        }
-      }
+      addKeys(allActions, module.actionNames);
     }
   }
+
+  addKeys(allActions, stripesComponents.stripes.actionNames);
 
   return Object.keys(allActions);
 }
