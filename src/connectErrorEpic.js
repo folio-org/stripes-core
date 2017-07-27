e09bcb2393dd6ccd3c2357be359278157d1b957b
Jason Skomorowski <jason@indexdata.com>
2017-07-27 11:41:51 -0400

    Show alert on stripes-connect errors, demonstrates STCON-25
    ...is adequately handled by the actions in STCON-20 + redux-observable.

---
+++ src/connectErrorEpic.js
@@ -0,0 +1,20 @@
+const connectErrorEpic = action$ => action$
+  .filter(action =>
+    action.type === '@@stripes-connect/FETCH_ERROR'
+    || action.type === '@@stripes-connect/MUTATION_ERROR')
+  .map((action) => {
+    const { meta, payload: e } = action;
+    const op = action.type === '@@stripes-connect/FETCH_ERROR' ? 'GET' : e.type;
+    // eslint-disable-next-line prefer-template,no-alert
+    window.alert(`ERROR: in module ${meta.module}, operation ${op}`
+      + ` on resource '${meta.resource}' failed`
+      + (action.payload.status ? ` with HTTP status ${e.status}` : '')
+      + (e.message ? `, saying: ${e.message}` : ''));
+
+    // TODO: When we have a more complete notification system and present our errors
+    // through it this will better follow the redux-observable pattern of emitting
+    // another action.
+    return { type: '@@stripes-core/CREATE_NOTIFICATION' };
+  });
+
+export default connectErrorEpic;
