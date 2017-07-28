db4f46f14d945bf6637f7d12c5d71483b138633b
Mike Taylor <mike@indexdata.com>
2017-07-28 18:04:50 +0100

    /sso-landing accepts "ssoToken" cookie as well as query parameter.
    To do this, we have added react-cookie as a dependency.
    
    Fixes STCOR-38.

--- src/components/SSOLanding.js
+++ src/components/SSOLanding.js
@@ -1,36 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
-import Link from 'react-router-dom/Link';
+import { withCookies, Cookies } from 'react-cookie';
 import queryString from 'query-string';
+import Link from 'react-router-dom/Link';
+
+function getCookie(props) {
+  const cookies = props.cookies;
+  const cookie = cookies.get('ssoToken');
+  return cookie;
+}
+
+function getParam(props) {
+  const search = props.location.search;
+  if (!search) return undefined;
+  const query = queryString.parse(search);
+  return query.ssoToken;
+}
 
 const SSOLanding = (props) => {
-  const search = props.location.search;
-  if (!search) {
-    return <div>No search string</div>;
-  }
-  const query = queryString.parse(search);
-  const token = query['ssoToken'];
-  if (!token) {
-    return <div>No <tt>ssoToken</tt> query parameter</div>;
+  const cookie = getCookie(props);
+  const param = getParam(props);
+
+  if (!cookie && !param) {
+    return <div>No <tt>ssoToken</tt> cookie or query parameter</div>;
+  } else if (cookie && param) {
+    return <div>Both <tt>ssoToken</tt> cookie and query parameters provided</div>;
   }
 
-  props.stripes.setToken(token);
+  props.stripes.setToken(cookie || param);
   return (
     <div>
-      <p>Logged in with: token <tt>{token}</tt></p>
+      <p>Logged in with token <tt>{cookie || param}</tt> from {cookie ? 'cookie' : 'param'}</p>
       <p><Link to="/">continue</Link></p>
     </div>
   );
 };
 
 SSOLanding.propTypes = {
+  // eslint-disable-next-line react/no-unused-prop-types
   location: PropTypes.shape({
     search: PropTypes.string,
   }).isRequired,
+  // eslint-disable-next-line react/no-unused-prop-types
+  cookies: PropTypes.instanceOf(Cookies).isRequired,
   stripes: PropTypes.shape({
     setToken: PropTypes.func.isRequired,
   }).isRequired,
 };
 
-export default withRouter(SSOLanding);
+export default withCookies(withRouter(SSOLanding));
