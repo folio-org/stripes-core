abbf7381fcbe975d15685576424f2226e2848fc7
Tanuja Gadde <tgadde@corp.epnet.com>
2017-08-01 16:13:36 -0400

    remove condition as per comments from jason

--- src/configureStore.js
+++ src/configureStore.js
@@ -7,18 +7,13 @@ import enhanceReducer from './enhanceReducer';
 import connectErrorEpic from './connectErrorEpic';
 
 export default function configureStore(initialState, config, stripesLogger) {
-  let createStoreWithMiddleware;
   const reducer = enhanceReducer(combineReducers(initialReducers));
   const middleware = applyMiddleware(thunk, epicMiddleware);
  /* eslint-disable no-underscore-dangle */
-  if (stripesLogger.hasCategory('redux')) {
-    createStoreWithMiddleware = compose(
+  const createStoreWithMiddleware = compose(
     middleware,
       window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
     );
-  } else {
-    createStoreWithMiddleware = compose(middleware);
-  }
   /* eslint-enable */
 
   addEpics([connectErrorEpic]);
