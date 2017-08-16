3faeb9504fafd938b075161273fe72bf8f37f622
Tanuja Gadde <tgadde@corp.epnet.com>
2017-08-16 11:40:24 -0400

    put back the reduc logger

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,10 +3,15 @@ import thunk from 'redux-thunk';
 import initialReducers from './initialReducers';
 import enhanceReducer from './enhanceReducer';
 import epics from './epics';
+import { createLogger } from 'redux-logger';
 
-export default function configureStore(initialState) {
+export default function configureStore(initialState, stripesLogger) {
+  const logger = createLogger({
+    // Show logging unless explicitly set false
+    predicate: () => stripesLogger.hasCategory('redux'),
+  });
   const reducer = enhanceReducer(combineReducers(initialReducers));
-  const middleware = applyMiddleware(thunk, epics.middleware);
+  const middleware = applyMiddleware(thunk, logger, epics.middleware);
  /* eslint-disable no-underscore-dangle */
   const createStoreWithMiddleware = compose(
     middleware,
