b094369c7bc1aee1d95b3d955238e26b9966629c
Wil Wilsman <wil@frontside.io>
2017-09-05 11:45:40 -0500

    Fix redux devtools compose
    The previous method of composing the middleware would call the
    middleware initializers twice, and so a side-effect of this was epics
    would receive double the actions.
    
    This changes the compose method to use redux devtool's own compose and
    thus fixes the double epic calls

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,19 +4,15 @@ import { createLogger } from 'redux-logger';
 import initialReducers from './initialReducers';
 import enhanceReducer from './enhanceReducer';
 
+const enhanceMiddleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default function configureStore(initialState, stripesLogger, epics) {
   const logger = createLogger({
     // Show logging unless explicitly set false
     predicate: () => stripesLogger.hasCategory('redux'),
   });
   const reducer = enhanceReducer(combineReducers(initialReducers));
-  const middleware = applyMiddleware(thunk, logger, epics.middleware);
- /* eslint-disable no-underscore-dangle */
-  const createStoreWithMiddleware = compose(
-    middleware,
-      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
-    );
-  /* eslint-enable */
+  const middleware = enhanceMiddleware(applyMiddleware(thunk, logger, epics.middleware));
 
-  return createStoreWithMiddleware(createStore)(reducer, initialState, middleware);
+  return createStore(reducer, initialState, middleware);
 }
