1a0975df756ab5e314be498b9f2cd88e2abf34d6
John Coburn <JohnC-80@users.noreply.github.com>
2017-09-25 00:07:11 -0500

    linting

--- src/components/MainNav/Notifications/NotificationsDropdown.js
+++ src/components/MainNav/Notifications/NotificationsDropdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import moment from 'moment';
+import moment from 'moment'; // eslint-disable-line import/no-extraneous-dependencies
 import { Dropdown } from '@folio/stripes-components/lib/Dropdown';
 import DropdownMenu from '@folio/stripes-components/lib/DropdownMenu';
 import NotificationsButton from './NotificationsButton';
@@ -12,7 +12,7 @@ class NotificationsDropdown extends React.Component {
     this.state = {
       dropdownOpen: false,
       lastOpen: null,
-    }
+    };
 
     this.handleToggle = this.handleToggle.bind(this);
   }
@@ -20,9 +20,9 @@ class NotificationsDropdown extends React.Component {
   handleToggle() {
     const newState = {};
     newState.dropdownOpen = !this.state.dropdownOpen;
-    
-    if(!newState.dropdownOpen) {
-      newState.lastOpen = new moment();
+
+    if (!newState.dropdownOpen) {
+      newState.lastOpen = moment();
     }
 
     this.setState(newState);
@@ -31,9 +31,9 @@ class NotificationsDropdown extends React.Component {
   render() {
     return (
       <Dropdown onToggle={this.handleToggle} open={this.state.dropdownOpen}>
-        <NotificationsButton data-role='toggle' title="Notifications" />
-        <DropdownMenu data-role='menu' onToggle={this.handleToggle}>
-          <NotificationsMenu  lastOpen={this.state.lastOpen}/>
+        <NotificationsButton data-role="toggle" title="Notifications" />
+        <DropdownMenu data-role="menu" onToggle={this.handleToggle}>
+          <NotificationsMenu lastOpen={this.state.lastOpen} />
         </DropdownMenu>
       </Dropdown>
     );
@@ -41,11 +41,3 @@ class NotificationsDropdown extends React.Component {
 }
 
 export default NotificationsDropdown;
-/*
-  <Dropdown onToggle={this.handleToggle}>
-    <NotificationsButton data-role='toggle' />
-    <DropdownMenu data-role='menu' onToggle={this.handleToggle}>
-      <NotificationsMenu  />
-    </DropdownMenu>
-  </Dropdown>
-*/
