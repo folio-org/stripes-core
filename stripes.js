9cad3b4eb61b5e2e6b42f42003c27b416384afd0
Hongwei Ji <hji@ebsco.com>
2017-10-02 10:37:26 -0400

    STCOR-80 Allow adjusting of output.publicPath

--- stripes.js
+++ stripes.js
@@ -90,15 +90,18 @@ commander
 
 commander
   .command('build')
-  .arguments('<config> <output>')
+  .arguments('<config> <output> [publicPath]')
   .description('Build a tenant bundle')
-  .action(function (stripesConfigFile, outputPath) {
+  .action(function (stripesConfigFile, outputPath, publicPath) {
     const config = require('./webpack.config.cli.prod');
     const stripesConfig = require(path.resolve(stripesConfigFile));
     config.plugins.push(new StripesConfigPlugin(stripesConfig));
     config.resolve.modules = ['node_modules', cwdModules];
     config.resolveLoader = { modules: ['node_modules', cwdModules] };
     config.output.path = path.resolve(outputPath);
+    if (publicPath) {
+      config.output.publicPath = publicPath;
+    }
     const compiler = webpack(config);
     compiler.run(function (err, stats) { 
       processStats(err, stats);
