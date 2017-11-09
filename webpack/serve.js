068b2e74037450fea5fdd6f4428a3d385422c8b5
Matt Jones <mattjones@ebsco.com>
2017-11-09 15:03:02 -0500

    Return a promise from serve()
    This enables chaining tasks upon the completion of webpack build. Primarily to be used by stripes-cli for integration tests.

--- webpack/serve.js
+++ webpack/serve.js
@@ -25,53 +25,56 @@ const cachePlugin = new HardSourceWebpackPlugin({
 });
 
 module.exports = function serve(stripesConfig, options) {
-  const app = express();
-  let config = require('../webpack.config.cli.dev'); // eslint-disable-line
+  return new Promise((resolve) => {
+    const app = express();
+    let config = require('../webpack.config.cli.dev'); // eslint-disable-line
 
-  config.plugins.push(new StripesConfigPlugin(stripesConfig));
+    config.plugins.push(new StripesConfigPlugin(stripesConfig));
 
-  // Look for modules in node_modules, then the platform, then stripes-core
-  config.resolve.modules = ['node_modules', platformModulePath, coreModulePath];
-  config.resolveLoader = { modules: ['node_modules', platformModulePath, coreModulePath] };
+    // Look for modules in node_modules, then the platform, then stripes-core
+    config.resolve.modules = ['node_modules', platformModulePath, coreModulePath];
+    config.resolveLoader = { modules: ['node_modules', platformModulePath, coreModulePath] };
 
-  if (options.cache) {
-    config.plugins.push(cachePlugin);
-  }
-  if (options.devtool) {
-    config.devtool = options.devtool;
-  }
-  // Give the caller a chance to apply their own webpack overrides
-  if (options.webpackOverrides && typeof options.webpackOverrides === 'function') {
-    config = options.webpackOverrides(config);
-  }
-
-  const compiler = webpack(config);
-
-  const port = options.port || process.env.STRIPES_PORT || 3000;
-  const host = options.host || process.env.STRIPES_HOST || 'localhost';
-
-  app.use(express.static(`${serverRoot}/public`));
-
-  // Process index rewrite before webpack-dev-middleware
-  // to respond with webpack's dist copy of index.html
-  app.use(connectHistoryApiFallback({}));
-
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath,
-  }));
-
-  app.use(webpackHotMiddleware(compiler));
-
-  app.get('/favicon.ico', (req, res) => {
-    res.sendFile(`${serverRoot}/favicon.ico`);
-  });
-
-  app.listen(port, host, (err) => {
-    if (err) {
-      console.log(err);
-      return;
+    if (options.cache) {
+      config.plugins.push(cachePlugin);
     }
-    console.log(`Listening at http://${host}:${port}`);
+    if (options.devtool) {
+      config.devtool = options.devtool;
+    }
+    // Give the caller a chance to apply their own webpack overrides
+    if (options.webpackOverrides && typeof options.webpackOverrides === 'function') {
+      config = options.webpackOverrides(config);
+    }
+
+    const compiler = webpack(config);
+    compiler.plugin('done', stats => resolve(stats));
+
+    const port = options.port || process.env.STRIPES_PORT || 3000;
+    const host = options.host || process.env.STRIPES_HOST || 'localhost';
+
+    app.use(express.static(`${serverRoot}/public`));
+
+    // Process index rewrite before webpack-dev-middleware
+    // to respond with webpack's dist copy of index.html
+    app.use(connectHistoryApiFallback({}));
+
+    app.use(webpackDevMiddleware(compiler, {
+      noInfo: true,
+      publicPath: config.output.publicPath,
+    }));
+
+    app.use(webpackHotMiddleware(compiler));
+
+    app.get('/favicon.ico', (req, res) => {
+      res.sendFile(`${serverRoot}/favicon.ico`);
+    });
+
+    app.listen(port, host, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`Listening at http://${host}:${port}`);
+    });
   });
 };
