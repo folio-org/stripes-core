a0e7c4d915f95fc5ae424187326056e5e555fbb0
William Welling <wwelling@library.tamu.edu>
2018-01-10 08:49:03 -0600

    reverting changes from uiu-357

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -60,41 +60,28 @@ class MainNav extends Component {
     this.toggleUserMenu = this.toggleUserMenu.bind(this);
     this.logout = this.logout.bind(this);
     this.lastVisited = {};
+    this.unsub = null;
     this.queryValues = null;
 
-    this.moduleName = undefined;
-    this.unsub = undefined;
-
-    this.moduleList = modules.app.concat({
+    const moduleList = modules.app.concat({
       route: '/settings',
       module: '@folio/x_settings',
     });
 
+    for (const entry of moduleList) {
+      if (props.location.pathname.startsWith(entry.route)) {
+        this.subscribeToQueryChanges(entry);
+      }
+    }
+
     props.history.listen((hist) => {
-      for (const entry of this.moduleList) {
+      for (const entry of moduleList) {
         if (hist.pathname === entry.route || hist.pathname.startsWith(`${entry.route}/`)) {
           const name = entry.module.replace(/^@folio\//, '');
           this.lastVisited[name] = `${hist.pathname}${hist.search}`;
         }
       }
     });
-  }
-
-  componentDidUpdate() {
-    for (const entry of this.moduleList) {
-      if (this.props.location.pathname.startsWith(entry.route)) {
-        const name = entry.module.replace(/^@folio\//, '');
-        if (this.moduleName !== name) {
-          if (this.unsub) {
-            this.unsub();
-          }
-          if (entry.queryResource) {
-            this.unsub = this.subscribeToQueryChanges(entry);
-          }
-          this.moduleName = name;
-        }
-      }
-    }
   }
 
   toggleUserMenu() {
@@ -113,7 +100,9 @@ class MainNav extends Component {
   }
 
   subscribeToQueryChanges(moduleInfo) {
-    return this.store.subscribe(() => {
+    if (this.unsub) this.unsub();
+    if (!moduleInfo.queryResource) return;
+    this.unsub = this.store.subscribe(() => {
       const previousQueryValues = this.queryValues;
       // This is not DRY, as it was expressed already in LocalResource is stripes-connect,
       // And in Root.js in stripes-core. Both State Keys should be derived from a common mechanism.
@@ -127,13 +116,22 @@ class MainNav extends Component {
         let url = allParams._path || location.pathname;
         delete allParams._path;
 
-        const nonNull = Object.keys(allParams).filter(k => allParams[k] != null).reduce((r, k) => Object.assign(r, { [k]: allParams[k] }), {});
+        const nonNull = Object.keys(allParams)
+          .filter(k => allParams[k] != null)
+          .reduce((r, k) => Object.assign(r, { [k]: allParams[k] }), {});
         if (Object.keys(nonNull).length) {
           url += `?${queryString.stringify(nonNull)}`;
         }
         this.props.history.replace(url);
       }
     });
+  }
+
+  handleNavigation(entry) {
+    return () => {
+      this.subscribeToQueryChanges(entry);
+      this.props.history.push(this.lastVisited[name] || entry.home || entry.route);
+    };
   }
 
   render() {
@@ -177,7 +175,7 @@ class MainNav extends Component {
 
       if (!stripes.hasPerm(perm)) return null;
 
-      return (<NavButton id={navId} selected={pathname.startsWith(entry.route)} href={this.lastVisited[name] || entry.home || entry.route} title={entry.displayName} key={entry.route}>
+      return (<NavButton id={navId} selected={pathname.startsWith(entry.route)} onClick={this.handleNavigation(entry)} href={this.lastVisited[name] || entry.home || entry.route} title={entry.displayName} key={entry.route}>
         <NavIcon color="#61f160" />
         <span className={css.linkLabel}>
           {entry.displayName}
@@ -202,7 +200,7 @@ class MainNav extends Component {
             <span className={css.brandingLabel} style={{ fontSize: '22px', lineHeight: '1rem' }}>FOLIO</span>
           </NavButton>
           {selectedApp &&
-            <NavButton href={this.lastVisited[name] || selectedApp.home} title={selectedApp.displayName} key="selected-app">
+            <NavButton onClick={this.handleNavigation(selectedApp)} href={this.lastVisited[name] || selectedApp.home} title={selectedApp.displayName} key="selected-app">
               <NavIcon color="#61f160" />
               <span className={css.linkLabel}>
                 {selectedApp.displayName}
