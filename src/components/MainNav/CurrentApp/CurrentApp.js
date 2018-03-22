2d52d7f3661fcf8c06b28da3eee62db015586baf
Rasmus Wølk <640976+rasmuswoelk@users.noreply.github.com>
2018-03-22 18:36:37 +0100

    Revert "Updated MainNav to be more responsive (UX-153)"

--- src/components/MainNav/CurrentApp/CurrentApp.js
+++ src/components/MainNav/CurrentApp/CurrentApp.js
@@ -17,6 +17,7 @@ const propTypes = {
     },
   ),
   id: PropTypes.string,
+  iconData: PropTypes.object,
   badge: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
@@ -27,13 +28,14 @@ const defaultProps = {
   currentApp: { displayName: 'FOLIO', description: 'FOLIO platform' },
 };
 
-const CurrentApp = ({ currentApp, id, badge }) => {
-  const { description, iconData, name, displayName } = currentApp;
+const CurrentApp = ({ currentApp, iconData, id, badge }) => {
+  const { displayName, description, module } = currentApp;
+  const iconKey = module && module.replace(/^@[a-z0-9_]+\//, '');
 
   return (
     <div id={id} title={description} className={css.currentApp}>
       {badge && (<Badge color="red" className={css.badge}>{badge}</Badge>)}
-      <AppIcon icon={iconData} app={name} className={css.icon} />
+      <AppIcon icon={iconData} app={iconKey} className={css.icon} />
       <Headline tag="h1" size="small" margin="none">{displayName}</Headline>
     </div>
   );
