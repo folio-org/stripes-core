f2596d3d61d9e714ef7954ccb37c8cee7c6b37e9
Mark Deutsch <mark.deutsch@duke.edu>
2018-06-15 13:07:22 -0400

    Added <AppRoute> component

---
+++ src/components/AppRoute/AppRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Route from 'react-router-dom/Route';
+
+import ErrorBoundary from '@folio/stripes-components/lib/ErrorBoundary';
+
+import TitleManager from '../TitleManager';
+
+class AppRoute extends React.Component {
+  static propTypes = {
+    displayName: PropTypes.string,
+    component: PropTypes.element,
+  }
+
+  render() {
+    const { displayName, component, ...rest } = this.props;
+
+    return (
+      <Route
+        {...rest}
+        component={() => (
+          <ErrorBoundary>
+            <TitleManager title={displayName} />
+            {component}
+          </ErrorBoundary>
+        )}
+      />
+    );
+  }
+}
+
+export default AppRoute;
