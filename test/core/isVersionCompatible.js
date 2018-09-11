74c305463c5fb49f366fe46606b00b30f14e4dee
Mike Taylor <mike@indexdata.com>
2018-09-11 15:57:08 +0100

    ESLint tweaks

--- test/core/isVersionCompatible.js
+++ test/core/isVersionCompatible.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-import { isVersionCompatible } from '../../src/discoverServices';
+const { isVersionCompatible } = require('../../src/discoverServices');
 
 function runTest(tests) {
   const title = tests.shift();
@@ -28,18 +28,18 @@ describe('the isVersionCompatible function', function () {
     [
       'major only',
       '3',
-      [ '3', '3.5', '3.6', '3.6.1', '3.4', '3.4.7' ], // should succeed
-      [ '2,', '2.9', '4', '4.0', '4.0.0' ], // should fail
+      ['3', '3.5', '3.6', '3.6.1', '3.4', '3.4.7'], // should succeed
+      ['2,', '2.9', '4', '4.0', '4.0.0'], // should fail
     ], [
       'major.minor',
       '3.5',
-      [ '3.5', '3.6', '3.10', '3.6.1' ], // should succeed
-      [ '2.9', '3.4', '3.4.7', '4.0', '4.0.0' ], // should fail
+      ['3.5', '3.6', '3.10', '3.6.1'], // should succeed
+      ['2.9', '3.4', '3.4.7', '4.0', '4.0.0'], // should fail
     ], [
       'major.minor.trivial',
       '3.5.2',
-      [ '3.5.2', '3.5.5', '3.5.11', '3.6.1' ], // should succeed
-      [ '2.9', '3.4', '3.4.7', '3.5', '3.5.1', '4.0', '4.0.0' ], // should fail
+      ['3.5.2', '3.5.5', '3.5.11', '3.6.1'], // should succeed
+      ['2.9', '3.4', '3.4.7', '3.5', '3.5.1', '4.0', '4.0.0'], // should fail
     ]
   ];
 
