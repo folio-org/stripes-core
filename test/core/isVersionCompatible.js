00bbd572f98a3b28a5e43e805548a4e70c85a41f
Mike Taylor <mike@indexdata.com>
2018-09-11 15:47:33 +0100

    Expand test coverage for isVersionCompatible
    Part of STCOR-249.

--- test/core/isVersionCompatible.js
+++ test/core/isVersionCompatible.js
@@ -28,20 +28,18 @@ describe('the isVersionCompatible function', function () {
     [
       'major only',
       '3',
-      // The "should succeed" tests all fail, presumably because this is just not supported
-      [ /*'3.5', '3.6', '3.6.1', '3.4', '3.4.7'*/ ], // should succeed
-      [ '2.9', '4.0', '4.0.0' ], // should fail
+      [ '3', '3.5', '3.6', '3.6.1', '3.4', '3.4.7' ], // should succeed
+      [ '2,', '2.9', '4', '4.0', '4.0.0' ], // should fail
     ], [
       'major.minor',
       '3.5',
-      [ '3.5', '3.6', '3.6.1' ], // should succeed
+      [ '3.5', '3.6', '3.10', '3.6.1' ], // should succeed
       [ '2.9', '3.4', '3.4.7', '4.0', '4.0.0' ], // should fail
     ], [
       'major.minor.trivial',
       '3.5.2',
-      [ '3.5.2', '3.5.5', '3.6.1' ], // should succeed
-      // XXX fix the two commented-out cased below
-      [ '2.9', '3.4', '3.4.7', /*'3.5', '3.5.1',*/ '4.0', '4.0.0' ], // should fail
+      [ '3.5.2', '3.5.5', '3.5.11', '3.6.1' ], // should succeed
+      [ '2.9', '3.4', '3.4.7', '3.5', '3.5.1', '4.0', '4.0.0' ], // should fail
     ]
   ];
 
