55800c64b35f4de6050d29fe2a03d0c97450d65e
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-09-13 15:38:46 -0500

    Start exporting things

---
+++ index.js
@@ -0,0 +1,8 @@
+export { RootContext } from './src/components/Root/RootContext';
+export { stripesShape } from './src/Stripes';
+export { withStripes } from './src/StripesContext';
+
+export { default as Pluggable } from './src/Pluggable';
+export { setServicePoints, setCurServicePoint } from './src/loginServices';
+export { default as TitleManager } from './src/components/TitleManager';
+export { default as coreEvents } from './src/events';
