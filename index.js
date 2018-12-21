4339e400efe7902cd43ca7ea4d5b7b4df3820a8d
John Coburn <jcoburn@ebsco.com>
2018-12-21 10:09:00 -0600

    add IntlConsumer component

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ export { default as IfPermission } from './src/components/IfPermission';
 export { default as TitleManager } from './src/components/TitleManager';
 export { default as HandlerManager } from './src/components/HandlerManager';
 export { default as coreEvents } from './src/events';
+export { default as IntlConsumer } from './src/components/IntlConsumer';
