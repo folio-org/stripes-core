7442685e5a1ae45e80d2378557e8ab59751a7f3b
John Coburn <jcoburn@ebsco.com>
2019-03-15 16:13:11 -0500

    re-export ConnectContext

--- src/components/Root/RootContext.js
+++ src/components/Root/RootContext.js
@@ -1,21 +1,24 @@
-import React from 'react';
+// import React from 'react';
 
-export const RootContext = React.createContext();
+// export const RootContext = React.createContext();
 
-function getDisplayName(WrappedComponent) {
-  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
-}
+// function getDisplayName(WrappedComponent) {
+//   return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+// }
 
-export function withRoot(WrappedComponent) {
-  class WithRoot extends React.Component {
-    render() {
-      return (
-        <RootContext.Consumer>
-          {root => <WrappedComponent {...this.props} root={root} /> }
-        </RootContext.Consumer>
-      );
-    }
-  }
-  WithRoot.displayName = `WithRoot(${getDisplayName(WrappedComponent)})`;
-  return WithRoot;
-}
+
+export { ConnectContext as RootContext, withConnect as withRoot } from '@folio/stripes-connect';
+
+// export function withRoot(WrappedComponent) {
+//   class WithRoot extends React.Component {
+//     render() {
+//       return (
+//         <RootContext.Consumer>
+//           {root => <WrappedComponent {...this.props} root={root} /> }
+//         </RootContext.Consumer>
+//       );
+//     }
+//   }
+//   WithRoot.displayName = `WithRoot(${getDisplayName(WrappedComponent)})`;
+//   return WithRoot;
+// }
