192808f003c6556464ff1e107d277336feac80e1
John Coburn <jcoburn@ebsco.com>
2019-03-21 14:05:17 -0500

    comment within the old RootContext location

--- src/components/Root/RootContext.js
+++ src/components/Root/RootContext.js
@@ -1,3 +1,6 @@
-// The context that lived here mainly provided a courier for redux-centric elements.
+/* The context that lived here mainly provided a courier for redux-centric elements.
+* it now is provided by stripes-connect. This is a temporary location of the export while some modules still import withRoot
+* directly from this file.
+*/
 
 export { ConnectContext as RootContext, withConnect as withRoot } from '@folio/stripes-connect';
