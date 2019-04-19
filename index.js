bd308d8a988dec793ec49a4d9c9aa0eddce2c16a
Ryan Berger <rberger@ebsco.com>
2019-04-19 14:52:59 -0400

    Added setupStripesCore
    and startMirage as default exports so they don't need to be addressed by path.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+export { ConnectContext as RootContext, withConnect as withRoot } from '@folio/stripes-connect';
+
 export { stripesShape } from './src/Stripes';
 export { withStripes } from './src/StripesContext';
 export { default as stripesConnect } from './src/stripesConnect';
-
 export { default as Pluggable } from './src/Pluggable';
 export { setServicePoints, setCurServicePoint } from './src/loginServices';
+export { default as coreEvents } from './src/events';
+
 export { default as AppContextMenu } from './src/components/MainNav/CurrentApp/AppContextMenu';
 export { default as IfInterface } from './src/components/IfInterface';
 export { default as IfPermission } from './src/components/IfPermission';
 export { default as TitleManager } from './src/components/TitleManager';
 export { default as HandlerManager } from './src/components/HandlerManager';
-export { default as coreEvents } from './src/events';
 export { default as IntlConsumer } from './src/components/IntlConsumer';
 export { default as AppIcon } from './src/components/AppIcon';
-export { ConnectContext as RootContext, withConnect as withRoot } from '@folio/stripes-connect';
 export { Route, Switch, Redirect } from './src/components/NestedRouter';
+
+export { default as setupStripesCore } from './test/bigtest/helpers/setup-application';
+export { default as startMirage } from './test/bigtest/network/start';
