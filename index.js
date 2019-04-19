9ead40bff605736fee885209cbdfa485646ef48e
Ryan Berger <rberger@ebsco.com>
2019-04-19 14:59:40 -0400

    Added comments

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+/* external utilities */
 export { ConnectContext as RootContext, withConnect as withRoot } from '@folio/stripes-connect';
 
+/* internal utilities */
 export { stripesShape } from './src/Stripes';
 export { withStripes } from './src/StripesContext';
 export { default as stripesConnect } from './src/stripesConnect';
@@ -7,6 +9,7 @@ export { default as Pluggable } from './src/Pluggable';
 export { setServicePoints, setCurServicePoint } from './src/loginServices';
 export { default as coreEvents } from './src/events';
 
+/* components */
 export { default as AppContextMenu } from './src/components/MainNav/CurrentApp/AppContextMenu';
 export { default as IfInterface } from './src/components/IfInterface';
 export { default as IfPermission } from './src/components/IfPermission';
@@ -16,5 +19,6 @@ export { default as IntlConsumer } from './src/components/IntlConsumer';
 export { default as AppIcon } from './src/components/AppIcon';
 export { Route, Switch, Redirect } from './src/components/NestedRouter';
 
+/* test scaffolding */
 export { default as setupStripesCore } from './test/bigtest/helpers/setup-application';
 export { default as startMirage } from './test/bigtest/network/start';
