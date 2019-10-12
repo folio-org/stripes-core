0d9043b4c8ae95aeee58084124630d8e10b88e05
Michal Kuklis <michal.kuklis@gmail.com>
2019-10-11 21:39:36 -0400

    Remove stripes-final-form dependency

--- src/components/ForgotPassword/ForgotPasswordForm.js
+++ src/components/ForgotPassword/ForgotPasswordForm.js
@@ -1,7 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Field } from 'react-final-form';
-import stripesFinalForm from '@folio/stripes-final-form';
+import { Field, Form } from 'react-final-form';
 import { FormattedMessage } from 'react-intl';
 import { isEmpty } from 'lodash';
 
@@ -20,8 +19,7 @@ import formStyles from './ForgotPasswordForm.css';
 
 class ForgotPasswordForm extends Component {
   static propTypes = {
-    pristine: PropTypes.bool.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
     errors: PropTypes.arrayOf(PropTypes.object),
   };
 
@@ -31,89 +29,89 @@ class ForgotPasswordForm extends Component {
 
   render() {
     const {
-      pristine,
-      handleSubmit,
       errors,
+      onSubmit
     } = this.props;
 
     return (
-      <Fragment>
-        <div className={formStyles.wrap}>
-          <div className={formStyles.centered}>
-            <OrganizationLogo />
-            <form
-              className={formStyles.form}
-              data-form="forgot"
-              onSubmit={handleSubmit}
-            >
-              <Headline
-                size="xx-large"
-                tag="h1"
-                data-test-h1
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, pristine }) => (
+          <div className={formStyles.wrap}>
+            <div className={formStyles.centered}>
+              <OrganizationLogo />
+              <form
+                className={formStyles.form}
+                data-form="forgot"
+                onSubmit={handleSubmit}
               >
-                <FormattedMessage id="stripes-core.label.forgotPassword" />
-              </Headline>
-              <Headline
-                size="large"
-                tag="p"
-                weight="regular"
-                faded
-                data-test-p
-              >
-                <FormattedMessage id="stripes-core.label.forgotPasswordCallToAction" />
-              </Headline>
-              <div className={formStyles.formGroup}>
-                <Field
-                  id="input-email-or-phone"
-                  component={TextField}
-                  name="userInput"
-                  type="text"
-                  marginBottom0
+                <Headline
+                  size="xx-large"
+                  tag="h1"
+                  data-test-h1
+                >
+                  <FormattedMessage id="stripes-core.label.forgotPassword" />
+                </Headline>
+                <Headline
+                  size="large"
+                  tag="p"
+                  weight="regular"
+                  faded
+                  data-test-p
+                >
+                  <FormattedMessage id="stripes-core.label.forgotPasswordCallToAction" />
+                </Headline>
+                <div className={formStyles.formGroup}>
+                  <Field
+                    id="input-email-or-phone"
+                    component={TextField}
+                    name="userInput"
+                    type="text"
+                    marginBottom0
+                    fullWidth
+                    inputClass={formStyles.input}
+                    validationEnabled={false}
+                    hasClearIcon={false}
+                    autoComplete="email-or-phone"
+                    autoCapitalize="none"
+                    autoFocus
+                  />
+                </div>
+                <Button
+                  buttonStyle="primary"
+                  id="clickable-login"
+                  name="continue-button"
+                  type="submit"
+                  buttonClass={formStyles.submitButton}
+                  disabled={pristine}
                   fullWidth
-                  inputClass={formStyles.input}
-                  validationEnabled={false}
-                  hasClearIcon={false}
-                  autoComplete="email-or-phone"
-                  autoCapitalize="none"
-                  autoFocus
-                />
-              </div>
-              <Button
-                buttonStyle="primary"
-                id="clickable-login"
-                name="continue-button"
-                type="submit"
-                buttonClass={formStyles.submitButton}
-                disabled={pristine}
-                fullWidth
-                marginBottom0
-                data-test-submit
-              >
-                <FormattedMessage id="stripes-core.button.continue" />
-              </Button>
-              <Row center="xs">
-                <Col xs={12}>
-                  <div
-                    className={formStyles.authErrorsWrapper}
-                    data-test-errors
-                  >
-                    {!isEmpty(errors) && (
+                  marginBottom0
+                  data-test-submit
+                >
+                  <FormattedMessage id="stripes-core.button.continue" />
+                </Button>
+                <Row center="xs">
+                  <Col xs={12}>
+                    <div
+                      className={formStyles.authErrorsWrapper}
+                      data-test-errors
+                    >
+                      {!isEmpty(errors) && (
                       <AuthErrorsContainer
                         errors={errors}
                         data-test-container
                       />
-                    )}
-                  </div>
-                </Col>
-              </Row>
-            </form>
+                      )}
+                    </div>
+                  </Col>
+                </Row>
+              </form>
+            </div>
           </div>
-        </div>
-      </Fragment>
+        )}
+      />
     );
   }
 }
 
-export default stripesFinalForm({
-  navigationCheck: false,
-})(ForgotPasswordForm);
+export default ForgotPasswordForm;
--- src/components/ForgotUserName/ForgotUserNameForm.js
+++ src/components/ForgotUserName/ForgotUserNameForm.js
@@ -1,7 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Field } from 'react-final-form';
-import stripesFinalForm from '@folio/stripes-final-form';
+import { Field, Form } from 'react-final-form';
 import { FormattedMessage } from 'react-intl';
 import { isEmpty } from 'lodash';
 
@@ -21,9 +20,8 @@ import formStyles from './ForgotUserNameForm.css';
 
 class ForgotUserNameForm extends Component {
   static propTypes = {
-    pristine: PropTypes.bool.isRequired,
     isValid: PropTypes.bool.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
     errors: PropTypes.arrayOf(PropTypes.object),
   };
 
@@ -42,8 +40,7 @@ class ForgotUserNameForm extends Component {
 
   render() {
     const {
-      pristine,
-      handleSubmit,
+      onSubmit,
       isValid,
       errors
     } = this.props;
@@ -51,86 +48,87 @@ class ForgotUserNameForm extends Component {
     const hasErrorsContainer = this.isErrorsContainerPresent();
 
     return (
-      <Fragment>
-        <div className={formStyles.wrap}>
-          <div className={formStyles.centered}>
-            <OrganizationLogo />
-            <form
-              className={formStyles.form}
-              data-form="forgot"
-              onSubmit={handleSubmit}
-            >
-              <Headline
-                size="xx-large"
-                tag="h1"
-                data-test-h1
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, pristine }) => (
+          <div className={formStyles.wrap}>
+            <div className={formStyles.centered}>
+              <OrganizationLogo />
+              <form
+                className={formStyles.form}
+                data-form="forgot"
+                onSubmit={handleSubmit}
               >
-                <FormattedMessage id="stripes-core.label.forgotUsername" />
-              </Headline>
-              <Headline
-                size="large"
-                tag="p"
-                weight="regular"
-                faded
-                data-test-p
-              >
-                <FormattedMessage id="stripes-core.label.forgotUsernameCallToAction" />
-              </Headline>
-              <div className={formStyles.formGroup}>
-                <Field
-                  id="input-email-or-phone"
-                  component={TextField}
-                  name="userInput"
-                  type="text"
-                  marginBottom0
+                <Headline
+                  size="xx-large"
+                  tag="h1"
+                  data-test-h1
+                >
+                  <FormattedMessage id="stripes-core.label.forgotUsername" />
+                </Headline>
+                <Headline
+                  size="large"
+                  tag="p"
+                  weight="regular"
+                  faded
+                  data-test-p
+                >
+                  <FormattedMessage id="stripes-core.label.forgotUsernameCallToAction" />
+                </Headline>
+                <div className={formStyles.formGroup}>
+                  <Field
+                    id="input-email-or-phone"
+                    component={TextField}
+                    name="userInput"
+                    type="text"
+                    marginBottom0
+                    fullWidth
+                    inputClass={formStyles.input}
+                    validationEnabled={false}
+                    hasClearIcon={false}
+                    autoComplete="email-or-phone"
+                    autoCapitalize="none"
+                    autoFocus
+                  />
+                </div>
+                <Button
+                  buttonStyle="primary"
+                  id="clickable-login"
+                  name="continue-button"
+                  type="submit"
+                  buttonClass={formStyles.submitButton}
+                  disabled={pristine}
                   fullWidth
-                  inputClass={formStyles.input}
-                  validationEnabled={false}
-                  hasClearIcon={false}
-                  autoComplete="email-or-phone"
-                  autoCapitalize="none"
-                  autoFocus
-                />
-              </div>
-              <Button
-                buttonStyle="primary"
-                id="clickable-login"
-                name="continue-button"
-                type="submit"
-                buttonClass={formStyles.submitButton}
-                disabled={pristine}
-                fullWidth
-                marginBottom0
-                data-test-submit
-              >
-                <FormattedMessage id="stripes-core.button.continue" />
-              </Button>
-              <Row center="xs">
-                <Col xs={12}>
-                  <div
-                    className={formStyles.authErrorsWrapper}
-                    data-test-errors
-                  >
-                    {hasErrorsContainer && (
-                      <AuthErrorsContainer
-                        errors={!isValid
-                          ? [{ code: forgotFormErrorCodes.EMAIL_INVALID }]
-                          : errors
-                        }
-                        data-test-container
-                      />
-                    )}
-                  </div>
-                </Col>
-              </Row>
-            </form>
+                  marginBottom0
+                  data-test-submit
+                >
+                  <FormattedMessage id="stripes-core.button.continue" />
+                </Button>
+                <Row center="xs">
+                  <Col xs={12}>
+                    <div
+                      className={formStyles.authErrorsWrapper}
+                      data-test-errors
+                    >
+                      {hasErrorsContainer && (
+                        <AuthErrorsContainer
+                          errors={!isValid
+                            ? [{ code: forgotFormErrorCodes.EMAIL_INVALID }]
+                            : errors
+                          }
+                          data-test-container
+                        />
+                      )}
+                    </div>
+                  </Col>
+                </Row>
+              </form>
+            </div>
           </div>
-        </div>
-      </Fragment>
+        )}
+      />
     );
   }
 }
 
-export default stripesFinalForm({
-  navigationCheck: false,
-})(ForgotUserNameForm);
+export default ForgotUserNameForm;
