565b5fada5859ee7b71dbfd9e8b8ca2ceafe2adc
Mark Deutsch <mark.deutsch@duke.edu>
2020-01-03 13:06:20 -0500

    Upgrade Babel from v6 to v7

--- webpack/babel-loader-rule.js
+++ webpack/babel-loader-rule.js
@@ -28,13 +28,28 @@ module.exports = {
   options: {
     cacheDirectory: true,
     presets: [
-      [require.resolve('babel-preset-env'), { modules: false }],
-      [require.resolve('babel-preset-stage-2')],
-      [require.resolve('babel-preset-react')],
+      ['@babel/preset-env'],
+      ['@babel/preset-flow', { all: true }],
+      ['@babel/preset-react'],
+      ['@babel/preset-typescript'],
     ],
     plugins: [
-      [require.resolve('babel-plugin-transform-decorators-legacy')],
-      [require.resolve('react-hot-loader/babel')]
+      // Stage 2
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-function-sent'],
+      ['@babel/plugin-proposal-export-namespace-from'],
+      ['@babel/plugin-proposal-numeric-separator'],
+      ['@babel/plugin-proposal-throw-expressions'],
+
+      // Stage 3
+      ['@babel/plugin-syntax-dynamic-import'],
+      ['@babel/plugin-syntax-import-meta'],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-json-strings'],
+
+      // Others
+      ['@babel/plugin-proposal-optional-chaining'],
+      ['react-hot-loader/babel'],
     ]
   },
 };
