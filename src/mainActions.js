fbd28576292f14a307c9c1b7293a504e067e2ddf
Michał Kuklis <michal.kuklis@gmail.com>
2020-01-16 20:11:41 -0500

    FOLIO-2097: Cleanup redux store during unmount (#789)

--- src/mainActions.js
+++ src/mainActions.js
@@ -4,8 +4,14 @@ function resetStore() {
   };
 }
 
+function destroyStore() {
+  return {
+    type: 'DESTROY_STORE',
+  };
+}
+
 // We export a single named function rather than using a default
 // export, to remain consistent with okapiActions.js
 //
 // eslint-disable-next-line import/prefer-default-export
-export { resetStore };
+export { resetStore, destroyStore };
