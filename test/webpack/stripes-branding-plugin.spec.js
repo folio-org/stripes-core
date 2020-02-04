247eb9555f373d13e74b7bbb48c9bc735878f653
Zak Burke <zburke@users.noreply.github.com>
2020-02-04 10:37:44 -0500

    STCOR-415 replace deprecated webapp-webpack-plugin (#798)
    We replaced `favicons-webpack-plugin` with `webapp-webpack-plugin`
    because the former had a memory leak, but the latter is now deprecated
    and has been folded back into the former.
    
    This requires that we update to a compatible version of 
    `html-webpack-plugin`, i.e. `^4.0.0-beta` as well. On the one hand I don't
    like the `-beta`, on the other, it's been in beta since August, 2018. That
    feels more like a permanent-beta of the "Gmail beta" sense and I don't 
    mind that at all. 
    
    Refs STCOR-415, STCOR-232, STCOR-296
    
    Co-authored-by: Matt Jones <mattjones@ebsco.com>

--- test/webpack/stripes-branding-plugin.spec.js
+++ test/webpack/stripes-branding-plugin.spec.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('webapp-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const defaultBranding = require('../../default-assets/branding');
 const StripesBrandingPlugin = require('../../webpack/stripes-branding-plugin');
 
@@ -29,9 +29,13 @@ const compilerStub = {
     },
     make: {
       tapAsync: () => {},
+      tapPromise: () => {},
     },
     compilation: {
       tap: () => {},
+    },
+    afterCompile: {
+      tapPromise: () => {},
     },
     emit: {
       tapAsync: () => {},
