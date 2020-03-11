358342c2c2e63bcceaeae5b8c5b68a850d2afdcd
Mike Taylor <mike@indexdata.com>
2020-03-11 13:33:54 +0000

    Okapi 509  optional dependencies (#819)
    * Extend package2md to support optionalOkapiInterfaces
    
    This is one of the Stripes parts of OKAPI-509, and allows a UI module to declare optional dependencies on Okapi interfaces. But we mostly use `stripes-cli` for this, so an analogous PR will be needed there, too.

--- util/package2md.js
+++ util/package2md.js
@@ -27,13 +27,19 @@ fs.readFile(filename, 'utf8', (err, data) => { // eslint-disable-line consistent
 
   const json = JSON.parse(data);
   const stripes = json.stripes || {};
-  const interfaces = stripes.okapiInterfaces || [];
   const md = {
     id: `${json.name.replace(/^@/, '').replace('/', '_')}-${json.version}`,
     name: json.description,
     permissionSets: stripes.permissionSets || [],
   };
-  if (strict) md.requires = Object.keys(interfaces).map(key => ({ id: key, version: interfaces[key] }));
+  if (strict) {
+    const interfaces = stripes.okapiInterfaces || [];
+    const optional = stripes.optionalOkapiInterfaces || [];
+    md.requires = [].concat(
+      Object.keys(interfaces).map(key => ({ id: key, version: interfaces[key] })),
+      Object.keys(optional).map(key => ({ id: key, version: optional[key], optional: true })),
+    );
+  }
 
   console.log(JSON.stringify(md, undefined, 2));
 });
