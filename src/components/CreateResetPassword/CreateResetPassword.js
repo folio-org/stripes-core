da3c5d09885a7ffefd0bef9d2545b89ca97d0996
Yelyzaveta Khokhlova <yelyzaveta_khokhlova@epam.com>
2020-09-11 14:11:54 +0300

    STCOR-441 removed comments

--- src/components/CreateResetPassword/CreateResetPassword.js
+++ src/components/CreateResetPassword/CreateResetPassword.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
-// import {
-//   reduxForm,
-//
-//   Form,
-//   formValueSelector,
-// } from 'redux-form';
 import {
   Field
 } from 'react-final-form';
