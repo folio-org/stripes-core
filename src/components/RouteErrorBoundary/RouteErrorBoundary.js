732ee516a3332da0d6845da84d2dca8c869eeee9
Zak Burke <zak.burke@gmail.com>
2021-02-01 15:37:39 -0500

    STCOR-455 publish ERROR events from RouteErrorBoundary (#1004)
    Publish `ERROR` events from `<RouteErrorBoundary>` and provide a
    callback to the `<ErrorBoundary>` component that allows its
    `componentDidCatch` method to provide error details to the modules that
    have registered to handle these `ERROR` events. (Modules of type
    `handler` are basically subscribers to events published by stripes-core.)
    
    While existing event-handlers return components to interrupt the render
    process, error logging is a bit different: we _don't_ want to interrupt
    that process; we just want to log the error. Hence the renaming of
    methods such as `getHandlerComponents` to `getEventHandlers` since the
    handlers may be components, or they may simply be functions.
    
    Refs STCOR-455, STCOR-493, STCOM-753

--- src/components/RouteErrorBoundary/RouteErrorBoundary.js
+++ src/components/RouteErrorBoundary/RouteErrorBoundary.js
@@ -2,17 +2,24 @@
  * RouteErrorBoundary
  */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import ErrorBoundary from '@folio/stripes-components/lib/ErrorBoundary';
+import events from '../../events';
+import { getEventHandlers } from '../../handlerService';
+import { ModulesContext } from '../../ModulesContext';
+import { StripesContext } from '../../StripesContext';
 
 const RouteErrorBoundary = ({ children, escapeRoute, moduleName, isSettings }) => {
   const intl = useIntl();
   const history = useHistory();
   const currentRoute = history.location.pathname;
   let buttonLabelId;
+
+  const modules = useContext(ModulesContext);
+  const stripes = useContext(StripesContext);
 
   if (moduleName) {
     if (isSettings) {
@@ -31,11 +38,25 @@ const RouteErrorBoundary = ({ children, escapeRoute, moduleName, isSettings }) =
     window.location.replace(resetPath);
   };
 
+  /**
+   * handleError
+   * Callback from ErrorBoundary's componentDidCatch method. Pass along
+   * the values received there to any functions that are registered to
+   * listen to events.ERROR.
+   */
+  const handleError = (error, info) => {
+    const handlers = getEventHandlers(events.ERROR, stripes, modules.handler, {});
+    handlers.forEach(handleEvent => {
+      handleEvent(error, info);
+    });
+  };
+
   return (
     <ErrorBoundary
       subTitle={intl.formatMessage({ id: 'stripes-core.routeErrorBoundary.sub' })}
       resetButtonLabel={intl.formatMessage({ id: buttonLabelId }, { name: moduleName })}
       onReset={handleReset}
+      onError={handleError}
     >
       {children}
     </ErrorBoundary>
