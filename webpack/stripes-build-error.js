53b245000e4d11a4490eeb5a84fa8f4f9212db72
Michał Kuklis <michal.kuklis@gmail.com>
2021-02-08 16:01:43 -0500

    STRIPES-730: remove webpack from stripes-core (#1003) (#1013)
    This reverts commit 5783e75bfd863fcce9720b07468e441e596aa97e.
    
    Co-authored-by: Zak Burke <zburke@cornell.edu>

--- webpack/stripes-build-error.js
+++
@@ -1,7 +0,0 @@
-module.exports = class StripesBuildError extends Error {
-  constructor(...args) {
-    super(...args);
-    this.name = this.constructor.name;
-    Error.captureStackTrace(this, StripesBuildError);
-  }
-};
