8df7a0231df67456117c6d7e2c88204f667b2e53
Michal Kuklis <michal.kuklis@gmail.com>
2021-04-08 13:04:34 -0400

    STCOR-534: Move CalloutContext back to RootWithIntl

--- src/RootWithIntl.js
+++ src/RootWithIntl.js
@@ -72,6 +72,7 @@ class RootWithIntl extends React.Component {
               noWrapper
             >
               <Provider store={stripes.store}>
+                <OverlayContainer />
                 <Router history={history}>
                   { token || disableAuth ?
                     <MainContainer>
@@ -83,7 +84,6 @@ class RootWithIntl extends React.Component {
                         />
                         { (stripes.okapi !== 'object' || stripes.discovery.isFinished) && (
                           <ModuleContainer id="content">
-                            <OverlayContainer />
                             <Switch>
                               <TitledRoute
                                 name="home"
