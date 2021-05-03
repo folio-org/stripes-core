84d5d7a0e75e241ec3dff34ebf11e61c7ccaae14
Michal Kuklis <michal.kuklis@gmail.com>
2021-05-03 13:28:32 -0400

    Cleanup

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ export { default as HandlerManager } from './src/components/HandlerManager';
 export { default as IntlConsumer } from './src/components/IntlConsumer';
 export { default as AppIcon } from './src/components/AppIcon';
 export { Route, Switch, Redirect } from './src/components/NestedRouter';
-export { useCurrentModule, CurrentModuleContext } from './src/components';
+export { CurrentModuleContext, useCurrentModule } from './src/components';
 
 /* misc */
 export { supportedLocales } from './src/loginServices';
