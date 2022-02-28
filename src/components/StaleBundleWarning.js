aa01b8d6d22b67a08d4892180ea9a8485da8d749
Jason Skomorowski <jason@indexdata.com>
2022-02-28 16:22:41 -0500

    Tidying and docs

--- src/components/StaleBundleWarning.js
+++ src/components/StaleBundleWarning.js
@@ -1,3 +1,24 @@
+/**
+ * Displays a banner warning users that a reload may be required based on a
+ * remote file or header having changed. This is configured in stripes.config.js
+ * via the staleBundleWarning property under the config section:
+ *
+ * staleBundleWarning - When present, this optional object will enable a
+ * periodic check to let users know when to reload the page because the bundle
+ * has changed. It takes the following properties:
+ *
+ * * path - path to fetch, this is the only required parameter
+ *
+ * * header - instead of the default to fetch and compare the body of a file
+ * (perhaps containing a build or commit id), use HTTP HEAD and the specified
+ * header
+ *
+ * * interval - how many minutes to wait between checks, defaults to 15
+ *
+ * eg. to compare the last-modified header of index.html every 5 minutes:
+ * { path: '/index.html', header: 'last-modified', interval: 5 }
+ */
+
 import { useQuery } from 'react-query';
 import ky from 'ky';
 import { useEffect, useState } from 'react';
@@ -11,6 +32,7 @@ const StaleBundleWarning = () => {
   const refetchInterval = (config?.interval ?? 15) * 60 * 1000;
   const [previous, setPrevious] = useState();
   const [stale, setStale] = useState(false);
+
   const query = useQuery({
     queryKey: ['StaleBundleWarning'],
     queryFn: async () => {
@@ -23,7 +45,7 @@ const StaleBundleWarning = () => {
           return await ky.get(config.path).text();
         }
       } catch (e) {
-        console.warn(`'Error checking for new bundle ${e}`); // eslint-disable-line no-console
+        console.warn(`Error checking for new bundle ${e}`); // eslint-disable-line no-console
         return null;
       }
     },
@@ -31,6 +53,7 @@ const StaleBundleWarning = () => {
     refetchInterval,
     enabled: !stale,
   });
+
   useEffect(() => {
     if (!previous) {
       setPrevious(query.data);
@@ -38,6 +61,7 @@ const StaleBundleWarning = () => {
     }
     if (previous !== query.data) setStale(true);
   }, [previous, query.data, stale]);
+
   return (
     <MessageBanner type="warning" show={stale}>
       <FormattedMessage id="stripes-core.stale.warning" />
