298d8ac0fa70893c11f99ec0afd9c6b288302c79
Zak Burke <zburke@ebsco.com>
2022-05-17 13:48:17 -0400

    restore non-extraneous timeout

--- test/bigtest/tests/CurrentApp-test.js
+++ test/bigtest/tests/CurrentApp-test.js
@@ -26,6 +26,12 @@ const DummyAppWithoutContextMenu = () => <div />;
 describe('CurrentApp', () => {
   const currentApp = new CurrentAppInteractor();
 
+  before(async () => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1900);
+    });
+  });
+
   setupApplication({
     modules: [
       {
