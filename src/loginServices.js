9c2e9b838e3d927b897650c2beb7f36d65062bc2
Zak Burke <zburke@ebsco.com>
2022-08-01 20:59:21 -0400

    STCOR-645 add cs_CZ to supported locales
    Add `cs_CZ` (Czech, Czechia) to the list of supported locales.
    
    Refs STCOR-645

--- src/loginServices.js
+++ src/loginServices.js
@@ -31,6 +31,7 @@ export const supportedLocales = [
   'ar',     // arabic
   'zh-CN',  // chinese, china
   'zh-TW',  // chinese, taiwan
+  'cs_CZ',  // czech, czechia
   'da-DK',  // danish, denmark
   'en-GB',  // british english
   'en-SE',  // english, sweden
