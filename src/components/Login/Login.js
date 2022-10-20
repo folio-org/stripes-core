92be0e4cd05dd5d53fa34b6006d8e2f59c268b81
John Coburn <JCoburn@EBSCO.COM>
2022-10-20 14:25:45 -0500

    remove unused import

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 
