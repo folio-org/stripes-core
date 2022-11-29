83a136319032992f0dca455117d0181fb1981a96
Michal Kuklis <michal.kuklis@gmail.com>
2022-11-28 23:52:40 -0500

    Fix index

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,5 @@ export { default as HandlerManager } from './HandlerManager';
 export * from './AppContextMenu';
 export { withModule, withModules } from './Modules';
 export { default as TitleManager } from './TitleManager';
-export { default as SSOLanding } from './SSOLanding';
-export { default as SSORedirect } from './SSORedirect';
 export * from './ModuleHierarchy';
 export * from './Namespace';
