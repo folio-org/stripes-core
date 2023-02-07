71ab7a574573941c858a8b73ea5bbed4a17934a8
Michal Kuklis <michal.kuklis@gmail.com>
2023-02-07 09:50:01 -0500

    Added more description

--- src/createReactQueryClient.js
+++ src/createReactQueryClient.js
@@ -1,5 +1,19 @@
 import { QueryClient, MutationCache } from 'react-query';
 
+/**
+  `MutationCache` is being used here to setup a global `onError` handler
+  in cases when the error happens during a mutation and the local `onError` handler
+  has not been provided via `useMutation`.
+
+  In order to avoid falling back to a default global onError please provide a local
+  onError handler e.g.:
+
+  useMutation(mutationFn, {
+    onError: (error) => {
+      handle the error here
+    }
+  });
+*/
 const mutationCache = new MutationCache({
   onError: (error, _variables, _context, mutation) => {
     // if local onError is not present
