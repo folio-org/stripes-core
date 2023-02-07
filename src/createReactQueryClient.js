8b8a575768a8684573f6c8bbd96cf8301a441e2d
Michal Kuklis <michal.kuklis@gmail.com>
2023-02-06 22:11:46 -0500

    STCOR-687: Configure global onError handler for React Query mutations

--- src/createReactQueryClient.js
+++ src/createReactQueryClient.js
@@ -1,8 +1,19 @@
-import { QueryClient } from 'react-query';
+import { QueryClient, MutationCache } from 'react-query';
+
+const mutationCache = new MutationCache({
+  onError: (error, _variables, _context, mutation) => {
+    // if local onError is not present
+    if (!mutation?.options?.onError) {
+      // eslint-disable-next-line no-alert
+      alert(error);
+    }
+  }
+});
 
 const cacheTimeMinutes = 5;
 
 const createReactQueryClient = () => new QueryClient({
+  mutationCache,
   // https://react-query.tanstack.com/guides/important-defaults
   defaultOptions: {
     queries: {
---
+++ src/createReactQueryClient.test.js
@@ -0,0 +1,46 @@
+import { useMutation, QueryClientProvider } from 'react-query';
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import createReactQueryClient from './createReactQueryClient';
+
+const mockMutation = jest.fn(() => {
+  throw new Error('Mutation failed');
+});
+
+const useMockedMutation = ({ onError }) => useMutation(mockMutation, { onError });
+
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={createReactQueryClient()}>
+    {children}
+  </QueryClientProvider>
+);
+
+describe('createReactQueryClient', () => {
+  beforeEach(() => {
+    window.alert = jest.fn();
+  });
+
+  afterEach(() => {
+    window.alert.mockClear();
+  });
+
+  it('skips the onError function when the onError option is passed to useMutation', async () => {
+    const { result } = renderHook(() => useMockedMutation({ onError: () => jest.fn() }), { wrapper });
+
+    act(() => {
+      result.current.mutate();
+    });
+
+    expect(window.alert).not.toHaveBeenCalled();
+  });
+
+  it('calls global onError function when the onError option is not passed to useMutation', async () => {
+    const { result } = renderHook(() => useMockedMutation({}), { wrapper });
+
+    await act(async () => {
+      await result.current.mutate();
+    });
+
+    expect(window.alert).toHaveBeenCalled();
+  });
+});
