6add08b9516f66067890a5c0f7534fc9e335d6c4
Michal Kuklis <michal.kuklis@gmail.com>
2023-04-25 21:40:31 -0400

    Cache remote components

--- src/loadRemoteComponent.js
+++ src/loadRemoteComponent.js
@@ -1,13 +1,14 @@
 // https://webpack.js.org/concepts/module-federation/#dynamic-remote-containers
 export default async function loadRemoteComponent(remoteUrl, remoteName) {
-  const container = await fetch(remoteUrl)
-    .then((res) => res.text())
-    .then((source) => {
-      const script = document.createElement('script');
-      script.textContent = source;
-      document.body.appendChild(script);
-      return window[remoteName];
-    });
+  if (!window[remoteName]) {
+    const response = await fetch(remoteUrl);
+    const source = await response.text();
+    const script = document.createElement('script');
+    script.textContent = source;
+    document.body.appendChild(script);
+  }
+
+  const container = window[remoteName];
 
   // eslint-disable-next-line no-undef
   await __webpack_init_sharing__('default');
@@ -17,5 +18,6 @@ export default async function loadRemoteComponent(remoteUrl, remoteName) {
 
   const factory = await container.get('./MainEntry');
   const Module = await factory();
+
   return Module;
 }
