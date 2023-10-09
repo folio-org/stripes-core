eabe2e99b75600be5cad303e75c9176b373216f1
Zak Burke <zburke@ebsco.com>
2023-10-09 12:40:48 -0400

    test cleanup

--- src/service-worker.test.js
+++ src/service-worker.test.js
@@ -10,18 +10,22 @@ import {
   rtr,
 } from './service-worker';
 
-// let consoleInterruptor = null;
 
-// beforeAll(() => {
-//   // reassign console.log to keep things quiet
-//   consoleInterruptor = console.log;
-//   console.log = () => { };
-// });
+global.fetch = jest.fn()
 
-// afterAll(() => {
-//   // restore console.log
-//   console.log = consoleInterruptor;
-// });
+// reassign console.log to keep things quiet
+const consoleInterruptor = {};
+beforeAll(() => {
+  consoleInterruptor.log = global.console.log;
+  consoleInterruptor.error = global.console.error;
+  console.log = () => { };
+  console.error = () => { };
+});
+
+afterAll(() => {
+  global.console.log = consoleInterruptor.log;
+  global.console.error = consoleInterruptor.error;
+});
 
 describe('isValidAT', () => {
   it('returns true for valid ATs', () => {
@@ -113,78 +117,6 @@ describe('messageToClient', () => {
   });
 });
 
-
-// /**
-//  * rtr
-//  * exchange an RT for a new one.
-//  * Make a POST request to /authn/refresh, including the current credentials,
-//  * and send a TOKEN_EXPIRATION event to clients that includes the new AT/RT
-//  * expiration timestamps.
-//  * @param {Event} event
-//  * @returns Promise
-//  * @throws if RTR fails
-//  */
-// const rtr = async (event) => {
-//   console.log('-- (rtr-sw) ** RTR ...');
-
-//   // if several fetches trigger rtr in a short window, all but the first will
-//   // fail because the RT will be stale after the first request rotates it.
-//   // the sentinel isRotating indicates that rtr has already started and therefore
-//   // should not start again; instead, we just need to wait until it finishes.
-//   // waiting happens in a for-loop that waits a few milliseconds and then rechecks
-//   // isRotating. hopefully, that process goes smoothly, but we'll give up after
-//   // IS_ROTATING_RETRIES * IS_ROTATING_INTERVAL milliseconds and return failure.
-//   if (isRotating) {
-//     for (let i = 0; i < IS_ROTATING_RETRIES; i++) {
-//       console.log(`-- (rtr-sw) **    is rotating; waiting ${IS_ROTATING_INTERVAL}ms`);
-//       await new Promise(resolve => setTimeout(resolve, IS_ROTATING_INTERVAL));
-//       if (!isRotating) {
-//         return Promise.resolve();
-//       }
-//     }
-//     // all is lost
-//     return Promise.reject(new Error('in-process RTR timed out'));
-//   }
-
-//   isRotating = true;
-//   return fetch(`${okapiUrl}/authn/refresh`, {
-//     headers: {
-//       'content-type': 'application/json',
-//       'x-okapi-tenant': okapiTenant,
-//     },
-//     method: 'POST',
-//     credentials: 'include',
-//     mode: 'cors',
-//   })
-//     .then(res => {
-//       if (res.ok) {
-//         return res.json();
-//       }
-
-//       // rtr failure. return an error message if we got one.
-//       return res.json()
-//         .then(json => {
-//           isRotating = false;
-
-//           if (json.errors[0]) {
-//             throw new Error(`${json.errors[0].message} (${json.errors[0].code})`);
-//           } else {
-//             throw new Error('RTR response failure');
-//           }
-//         });
-//     })
-//     .then(json => {
-//       console.log('-- (rtr-sw) **     success!');
-//       isRotating = false;
-//       tokenExpiration = {
-//         atExpires: new Date(json.accessTokenExpiration).getTime(),
-//         rtExpires: new Date(json.refreshTokenExpiration).getTime(),
-//       };
-//       messageToClient(event, { type: 'TOKEN_EXPIRATION', tokenExpiration });
-//     });
-// };
-
-
 describe('isPermissibleRequest', () => {
   it('accepts endpoints when AT is valid', () => {
     const req = { url: 'monkey' };
@@ -251,22 +183,6 @@ describe('isPermissibleRequest', () => {
   });
 });
 
-// /**
-//  * isLogoutRequest
-//  * Logout requests are always permissible but need special handling
-//  * because they should never fail.
-//  *
-//  * @param {Request} req clone of the original event.request object
-//  * @returns boolean true if the request URL matches a logout URL
-//  */
-// const isLogoutRequest = (req) => {
-//   const permissible = [
-//     '/authn/logout',
-//   ];
-
-//   // console.log(`-- (rtr-sw) logout request ${req.url}`);
-//   return !!permissible.find(i => req.url.startsWith(`${okapiUrl}${i}`));
-// };
 describe('isLogoutRequest', () => {
   describe('accepts logout endpoints', () => {
     it('/authn/logout', () => {
@@ -296,80 +212,6 @@ describe('isOkapiRequest', () => {
     expect(isOkapiRequest(req, 'https://bar.edu')).toBe(false);
   });
 });
-
-
-// /**
-//  * passThroughWithRT
-//  * Perform RTR then return the original fetch. on error, post an RTR_ERROR
-//  * message to clients and return an empty response in a resolving promise.
-//  *
-//  * @param {Event} event
-//  * @returns Promise
-//  */
-// const passThroughWithRT = (event) => {
-//   const req = event.request.clone();
-//   return rtr(event)
-//     .then(() => {
-//       console.log('-- (rtr-sw) => post-rtr-fetch', req.url);
-//       return fetch(event.request, { credentials: 'include' });
-//     })
-//     .catch((rtre) => {
-//       // kill me softly: send an empty response body, which allows the fetch
-//       // to return without error while the clients catch up, read the RTR_ERROR
-//       // and handle it, hopefully by logging out.
-//       // Promise.reject() here would result in every single fetch in every
-//       // single application needing to thoughtfully handle RTR_ERROR responses.
-//       messageToClient(event, { type: 'RTR_ERROR', error: rtre });
-//       return Promise.resolve(new Response({}));
-//     });
-// };
-
-// /**
-//  * passThroughWithAT
-//  * Given we believe the AT to be valid, pass the fetch through.
-//  * If it fails, maybe our beliefs were wrong, maybe everything is wrong,
-//  * maybe there is no God, or there are many gods, or god is a she, or
-//  * she is a he, or Lou Reed is god. Or maybe we were just wrong about the
-//  * AT and we need to conduct token rotation, so try that. If RTR succeeds,
-//  * yay, pass through the fetch as we originally intended because now we
-//  * know the AT will be valid. If RTR fails, then it doesn't matter about
-//  * Lou Reed. He may be god. We're still throwing an Error.
-//  * @param {Event} event
-//  * @returns Promise
-//  * @throws if any fetch fails
-//  */
-// const passThroughWithAT = (event) => {
-//   console.log('-- (rtr-sw)    (valid AT or authn request)');
-//   return fetch(event.request, { credentials: 'include' })
-//     .then(response => {
-//       if (response.ok) {
-//         return response;
-//       } else {
-//         // we thought the AT was valid but it wasn't, so try again.
-//         // if we fail this time, we're done.
-//         console.log('-- (rtr-sw)    (whoops, invalid AT; retrying)');
-//         return passThroughWithRT(event);
-//       }
-//     });
-// };
-
-// /**
-//  * passThroughLogout
-//  * The logout request should never fail, even if it fails.
-//  * That is, if it fails, we just pretend like it never happened
-//  * instead of blowing up and causing somebody to get stuck in the
-//  * logout process.
-//  * @param {Event} event
-//  * @returns Promise
-//  */
-// const passThroughLogout = (event) => {
-//   console.log('-- (rtr-sw)    (logout request)');
-//   return fetch(event.request, { credentials: 'include' })
-//     .catch(e => {
-//       console.error('-- (rtr-sw) logout failure', e); // eslint-disable-line no-console
-//       return Promise.resolve();
-//     });
-// };
 
 describe('passThroughLogout', () => {
   it('succeeds', async () => {
@@ -406,7 +248,7 @@ describe('passThrough', () => {
           clone: () => req,
         }
       };
-      const tokenExpiration = { }
+      const tokenExpiration = {};
       const oUrl = 'https://okapi.edu';
 
       const response = 'kenough';
@@ -425,7 +267,7 @@ describe('passThrough', () => {
           clone: () => req,
         }
       };
-      const tokenExpiration = { }
+      const tokenExpiration = {};
       const oUrl = 'https://okapi.edu';
 
       const error = 'not kenough';
@@ -579,7 +421,7 @@ describe('passThrough', () => {
       global.fetch = jest.fn()
         .mockReturnValueOnce(Promise.resolve({
           ok: false,
-          json: () => Promise.reject('RTR response failure')
+          json: () => Promise.reject(new Error('RTR response failure')),
         }))
         .mockReturnValueOnce(Promise.resolve(error));
 
