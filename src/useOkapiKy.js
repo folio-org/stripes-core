976eeb2c4436be7700122f2a8067dd0c4a48d5af
Zak Burke <zburke@ebsco.com>
2023-10-30 22:45:41 -0400

    STCOR-671 handle access-control via cookies (#1346)
    Handle access-control via HTTP-only cookies instead of storing the JWT
    in local storage and providing it in the `X-Okapi-Token` header of fetch
    requests, and proxy all requests through a service worker that performs
    Refresh Token Rotation as needed to make sure the access-token remains
    fresh.
    
    Notable changes:
    
    * Fetch requests are proxied by a Service Worker that intercepts them,
      validates that the Access Token is still valid, and performs token
      rotation (if it is not) before completing the original fetch.
    * Sessions automatically end (i.e. the user is automatically logged out)
      when the Refresh Token expires.
    * Access control is managed by including an HTTP-only cookie with all
      requests. This means the Access Token formerly available in the
      response-header as `X-Okapi-Token` is never accessible to JS code.
    
    * Requires folio-org/stripes-connect/pull/223
    * Requires folio-org/stripes-smart-components/pull/1397
    * Requires folio-org/stripes-webpack/pull/125
    
    Replaces #1340. It was gross and I really don't want to talk about it.
    Let us never mention it again.
    
    Refs STCOR-671, FOLIO-3627
    
    (cherry picked from commit 27d29484c907f059c090aaa3a8fda2af753e8049)

--- src/useOkapiKy.js
+++ src/useOkapiKy.js
@@ -2,19 +2,20 @@ import ky from 'ky';
 import { useStripes } from './StripesContext';
 
 export default () => {
-  const { locale = 'en', timeout = 30000, tenant, token, url } = useStripes().okapi;
+  const { locale = 'en', timeout = 30000, tenant, url } = useStripes().okapi;
 
   return ky.create({
-    prefixUrl: url,
+    credentials: 'include',
     hooks: {
       beforeRequest: [
         request => {
           request.headers.set('Accept-Language', locale);
           request.headers.set('X-Okapi-Tenant', tenant);
-          request.headers.set('X-Okapi-Token', token);
         }
       ]
     },
+    mode: 'cors',
+    prefixUrl: url,
     retry: 0,
     timeout,
   });
