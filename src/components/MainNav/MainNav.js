354a246f3296d5046abfac3065717389227c9cce
Zak Burke <zburke@ebsco.com>
2023-11-09 07:05:21 -0500

    STCOR-761 allow console to be preserved on logout
    Normally the console is cleared on logout. This is good for security but
    bad for debugging in the case where a user may be automatically logged
    out due to RTR.
    
    Refs STCOR-761

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -126,7 +126,9 @@ class MainNav extends Component {
 
   // return the user to the login screen, but after logging in they will be brought to the default screen.
   logout() {
-    console.clear(); // eslint-disable-line no-console
+    if (!config.preserveConsole) {
+      console.clear(); // eslint-disable-line no-console
+    }
     this.returnToLogin().then(() => {
       this.props.history.push('/');
     });
