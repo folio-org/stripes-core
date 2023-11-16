391a615b6a5d0c46150bd0e087a91dc5cbeaea4d
Zak Burke <zburke@ebsco.com>
2023-11-16 15:08:46 -0500

    clean up comments and resource handling
    * comments are in sync with function arguments
    * consistently handle non-string arguments passed to fetch

--- src/components/Root/Errors.js
+++ src/components/Root/Errors.js
@@ -1,15 +1,35 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable max-classes-per-file */
+
+/**
+ * RTRError
+ * Error occured during rotation
+ */
 export class RTRError extends Error {
-	constructor(response) {
-		// Set the message to the status text, such as Unauthorized,
-		// with some fallbacks. This message should never be undefined.
-		super(
-			response.statusText ||
-			String(
-				(response.status === 0 || response.status) ?
-					response.status : 'Unknown Refresh Token Error'
-			)
-		);
-		this.name = 'RTRError';
-		this.response = response;
-	}
+  constructor(response) {
+    // Set the message to the status text, such as Unauthorized,
+    // with some fallbacks. This message should never be undefined.
+    super(
+      response.statusText ||
+      String(
+        (response.status === 0 || response.status) ?
+          response.status : 'Unknown Refresh Token Error'
+      )
+    );
+    this.name = 'RTRError';
+    this.response = response;
+  }
+}
+
+/**
+ * UnexpectedResourceError
+ * Thrown when
+ */
+export class UnexpectedResourceError extends Error {
+  constructor(resource) {
+    super('Expected a string, URL, or Request but did not receive one.');
+
+    this.name = 'UnexpectedResourceError';
+    this.resource = resource;
+  }
 }
