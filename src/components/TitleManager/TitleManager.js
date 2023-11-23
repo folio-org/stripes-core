511314420e2dfef5a90c92d6289a5f02beae6065
Denys Bohdan <denys_bohdan@epam.com>
2023-11-23 16:18:41 +0100

    STCOR-767 Fix duplicated FOLIO in document title in some cases

--- src/components/TitleManager/TitleManager.js
+++ src/components/TitleManager/TitleManager.js
@@ -22,14 +22,24 @@ class TitleManager extends React.Component {
 
   renderTitle = (currentTitle) => {
     const { prefix, page, record } = this.props;
+    const postfix = (this.props.stripes.config || {}).platformName || APP;
 
     if (typeof currentTitle !== 'string') return '';
 
     const tokens = currentTitle.split(' - ');
+
+    /**
+     * When there are 2 items - that means there are page and postfix.
+     * If we don't clear the tokens[1] item then we'll have two postfixes - in tokens[1] and tokens[2] will be added later
+     */
+    if (tokens.length === 2) {
+      tokens[1] = '';
+    }
+
     if (page) tokens[0] = page;
     if (record) tokens[1] = record;
 
-    tokens[2] = (this.props.stripes.config || {}).platformName || APP;
+    tokens[2] = postfix;
 
     return prefix + tokens
       .filter(t => t)
