1e6b224ecff0102e2d032cccf2fdc1e7dc14c560
John Coburn <jcoburn@ebsco.com>
2023-11-27 10:00:35 -0600

    apply same settings to withOkapiKy as useOkapiKy
    (cherry picked from commit 6284e38e2bc148c06306a2fabd0c296843c8ac25)

--- src/withOkapiKy.js
+++ src/withOkapiKy.js
@@ -10,22 +10,29 @@ const withOkapiKy = (WrappedComponent) => {
         okapi: PropTypes.shape({
           tenant: PropTypes.string.isRequired,
           url: PropTypes.string.isRequired,
+          timeout: PropTypes.number,
+          locale: PropTypes.string,
         }).isRequired,
       }).isRequired,
     };
 
     constructor(props) {
       super();
-      const { tenant, url } = props.stripes.okapi;
+      const { tenant, url, timeout = 30000, locale = 'en' } = props.stripes.okapi;
       this.okapiKy = ky.create({
+        credentials: 'include',
         prefixUrl: url,
         hooks: {
           beforeRequest: [
             request => {
               request.headers.set('X-Okapi-Tenant', tenant);
+              request.headers.set('Accept-Language', locale);
             }
           ]
-        }
+        },
+        mode: 'cors',
+        retry: 0,
+        timeout,
       });
     }
 
