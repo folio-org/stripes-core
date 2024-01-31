807f8fe741e885530ac8b100b085d235c5fd00d7
Zak Burke <zburke@ebsco.com>
2024-01-31 13:07:00 -0500

    STCOR-796 replace x-okapi-token credentials with RTR and cookies (#1410)
    Move auth tokens into HTTP-only cookies and implement refresh token
    rotation (STCOR-671) by overriding global.fetch and
    global.XMLHttpRequest, disabling login when cookies are disabled
    (STCOR-762). This functionality is implemented behind an opt-in
    feature-flag (STCOR-763).
    
    Okapi and Keycloak do not handle the same situations in the same ways.
    Changes from the original implementation in PR #1376:
    
    * When a token is missing:
      * Okapi sends a 400 `text/plain` response
      * Keycloak sends a 401 `application/json` response
    * Keycloak authentication includes the extra step of exchanging the OTP
      for the AT/RT and that request needs the `credentials` and `mode`
      options
    * Some `loginServices` functions now retrieve the host the access from
      the `stripes-config` import instead of a function argument
    * always permit `/authn/token` requests to go through
    
    Refs STCOR-796, STCOR-671
    
    (cherry picked from commit 036135333b1bc7b7f52d518506b88eb7446f1358)

--- src/loginServices.js
+++ src/loginServices.js
@@ -292,8 +292,8 @@ export function getUserLocale(okapiUrl, store, tenant, userId) {
  */
 export function getPlugins(okapiUrl, store, tenant) {
   return fetch(`${okapiUrl}/configurations/entries?query=(module==PLUGINS)`, {
-    headers: getHeaders(tenant, store.getState().okapi.token),
     credentials: 'include',
+    headers: getHeaders(tenant, store.getState().okapi.token),
     mode: 'cors',
   })
     .then((response) => {
@@ -385,7 +385,7 @@ function loadResources(store, tenant, userId) {
 
 /**
  * spreadUserWithPerms
- * return an object { user, perms } based on response from bl-users/self.
+ * return an object { user, perms } based on response from .../_self.
  *
  * @param {object} userWithPerms
  *
@@ -455,9 +455,9 @@ export async function logout(okapiUrl, store) {
  * Dispatch the session object, then return a Promise that fetches
  * and dispatches tenant resources.
  *
- * @param {object} store
- * @param {string} tenant
- * @param {string} token
+ * @param {object} store redux store
+ * @param {string} tenant tenant name
+ * @param {string} token access token [deprecated; prefer folioAccessToken cookie]
  * @param {*} data
  *
  * @returns {Promise}
@@ -476,7 +476,7 @@ export function createOkapiSession(store, tenant, token, data) {
 
   store.dispatch(setCurrentPerms(perms));
 
-  // if we can't parse tokenExpiration data, e.g. because data comes from `/bl-users/_self`
+  // if we can't parse tokenExpiration data, e.g. because data comes from `.../_self`
   // which doesn't provide it, then set an invalid AT value and a near-future (+10 minutes) RT value.
   // the invalid AT will prompt an RTR cycle which will either give us new AT/RT values
   // (if the RT was valid) or throw an RTR_ERROR (if the RT was not valid).
@@ -625,7 +625,7 @@ export function handleLoginError(dispatch, resp) {
 /**
  * processOkapiSession
  * create a new okapi session with the response from either a username/password
- * authentication request or a bl-users/_self request.
+ * authentication request or a .../_self request.
  * response body is shaped like
  * {
     'access_token': 'SOME_STRING',
@@ -665,7 +665,7 @@ export function processOkapiSession(store, tenant, resp, ssoToken) {
 
 /**
  * validateUser
- * return a promise that fetches from bl-users/self.
+ * return a promise that fetches from .../_self.
  * if successful, dispatch the result to create a session
  * if not, clear the session and token.
  *
@@ -678,7 +678,9 @@ export function processOkapiSession(store, tenant, resp, ssoToken) {
  */
 export function validateUser(okapiUrl, store, tenant, session) {
   const { token, user, perms, tenant: sessionTenant = tenant } = session;
-  return fetch(`${okapiUrl}/bl-users/_self`, {
+  const usersPath = okapi.authnUrl ? 'users-keycloak' : 'bl-users';
+
+  return fetch(`${okapiUrl}/${usersPath}/_self`, {
     headers: getHeaders(sessionTenant, token),
     credentials: 'include',
     mode: 'cors',
@@ -707,7 +709,7 @@ export function validateUser(okapiUrl, store, tenant, session) {
           token,
           tokenExpiration,
         }));
-        return loadResources(okapiUrl, store, sessionTenant, user.id);
+        return loadResources(store, sessionTenant, user.id);
       });
     } else {
       return logout(okapiUrl, store);
