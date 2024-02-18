0adf6800fef5b206ce0b885d7fbf4d7f15609b8a
Zak Burke <zburke@ebsco.com>
2024-02-18 07:15:01 -0500

    Revert "STCOR-810 cleanup deprecated entitlement params (#1418)" (#1427)
    This reverts commit 13b9dc48f7da6c81072edda5c2732f252a997a12.
    
    The conditional here checked `okapi.tenantOptions`; it must check `config.tenantOptions`.

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -36,7 +36,7 @@ jest.mock('./AboutUIModuleDetails', () => () => 'AboutUIModuleDetails');
 jest.mock('./WarningBanner', () => () => 'WarningBanner');
 
 jest.mock('stripes-config', () => ({
-  okapi: { tenantOptions: true },
+  okapi: { tenantEntitlementUrl: true },
 }));
 
 // set query retries to false. otherwise, react-query will thoughtfully
