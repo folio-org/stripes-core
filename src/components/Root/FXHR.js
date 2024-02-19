0cf89105a5932c0ecf54af2587f0686bc0cdde3d
John Coburn <jcoburn@ebsco.com>
2024-02-19 15:55:09 -0600

    add token checks to XHR prior to rotating token

--- src/components/Root/FXHR.js
+++ src/components/Root/FXHR.js
@@ -1,5 +1,9 @@
 import { okapi } from 'stripes-config';
-import { isFolioApiRequest, rtr } from './token-util';
+import { isFolioApiRequest, rtr, isValidAT, isValidRT } from './token-util';
+import {
+  RTR_ERROR_EVENT,
+} from './Events';
+import { getTokenExpiry } from '../../loginServices';
 
 export default (deps) => {
   return class FXHRClass extends XMLHttpRequest {
@@ -16,12 +20,24 @@ export default (deps) => {
     }
 
     send = async (payload) => {
+      const { tokenExpiration, logger } = this.FFetchContext;
       this.FFetchContext.logger?.log('rtr', 'capture XHR send');
       if (this.shouldEnsureToken) {
-        await rtr(this.FFetchContext)
-          .then(() => {
-            super.send(payload);
-          });
+        if (!isValidAT(tokenExpiration, logger)) {
+          logger.log('rtr', 'local tokens expired; fetching from storage for XHR..');
+          this.FFetchContext.tokenExpiration = await getTokenExpiry();
+        }
+
+        if (isValidRT(this.FFetchContext.tokenExpiration, logger)) {
+          logger.log('rtr', 'local refresh token valid, sending XHR...');
+          super.send(payload);
+        } else {
+          await rtr(this.FFetchContext)
+            .then(() => {
+              logger.log('rtr', 'local token refreshed, sending XHR...');
+              super.send(payload);
+            });
+        }
       } else {
         super.send(payload);
       }
