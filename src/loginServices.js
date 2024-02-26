848af4fd56e258b8bfa5ebf4e47d4508421c6026
Zak Burke <zburke@ebsco.com>
2024-02-26 13:39:07 -0500

    STCOR-816 only fetch /saml/check when login-saml is present (#1432)
    * STCOR-816 only fetch /saml/check when login-saml is present
    
    When restoring an existing session, after discovery, do not fetch from
    `/saml/check` unless the `login-saml` interface (indicating SSO/SAML is
    available). The 404 clutters the log.
    
    Refs STCOR-816
    
    * Missing semicolon
    
    ---------
    
    Co-authored-by: Ryan Berger <rberger@ebsco.com>

--- src/loginServices.js
+++ src/loginServices.js
@@ -455,7 +455,7 @@ export async function logout(okapiUrl, store) {
     .then(localforage.removeItem('loginResponse'))
     .catch((error) => {
       // eslint-disable-next-line no-console
-      console.log(`Error logging out: ${JSON.stringify(error)}`)
+      console.log(`Error logging out: ${JSON.stringify(error)}`);
     });
 }
 
@@ -754,7 +754,10 @@ export function checkOkapiSession(okapiUrl, store, tenant) {
       return sess !== null ? validateUser(okapiUrl, store, tenant, sess) : null;
     })
     .then(() => {
-      return getSSOEnabled(okapiUrl, store, tenant);
+      if (store.getState().discovery?.interfaces?.['login-saml']) {
+        return getSSOEnabled(okapiUrl, store, tenant);
+      }
+      return Promise.resolve();
     })
     .finally(() => {
       store.dispatch(setOkapiReady());
