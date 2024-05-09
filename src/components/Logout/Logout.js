03f1c129a22b2fac52936158fae26b72767b8cfc
Zak Burke <zburke@ebsco.com>
2024-05-09 14:33:38 -0400

    Logout cleanup
    We only want logout functions to run once, so useEffect needs to have an
    empty dependency array. We don't care about changes to history or
    stripes, we're logging out.

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router';
+import { FormattedMessage } from 'react-intl';
 
 import { useStripes } from '../../StripesContext';
 import { getLocale, logout } from '../../loginServices';
@@ -18,13 +19,20 @@ const Logout = ({ history }) => {
   const stripes = useStripes();
   const [didLogout, setDidLogout] = useState(false);
 
-  useEffect(() => {
-    getLocale(stripes.okapi.url, stripes.store, stripes.okapi.tenant)
-      .then(logout(stripes.okapi.url, stripes.store, history))
-      .then(setDidLogout(true));
-  }, [history, stripes]);
+  useEffect(
+    () => {
+      getLocale(stripes.okapi.url, stripes.store, stripes.okapi.tenant)
+        .then(logout(stripes.okapi.url, stripes.store, history))
+        .then(setDidLogout(true));
+    },
+    // no dependencies because we only want to start the logout process once.
+    // we don't care about changes to history or stripes; certainly those
+    // could be updated as part of the logout process
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
 
-  return (didLogout ? <Redirect to="/" /> : <h1>LOGGED OUT YO</h1>);
+  return didLogout ? <Redirect to="/" /> : <FormattedMessage id="stripes-core.logoutPending" />;
 };
 
 Logout.propTypes = {
