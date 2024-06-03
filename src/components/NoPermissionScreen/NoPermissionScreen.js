d1cdfa8a14626235d52f7eb1ddec1cffb3f0b73e
Zak Burke <zburke@ebsco.com>
2024-06-03 10:43:44 -0400

    STCOR-842 refactor away from deprecated getChildContext (#1481)
    `getChildContext` will be deprecated in React v19. `AddContext` can be
    eliminated completely; it duplicates the functionality offered by
    `StripesContext`.
    
    Refs STCOR-842

--- src/components/NoPermissionScreen/NoPermissionScreen.js
+++ src/components/NoPermissionScreen/NoPermissionScreen.js
@@ -3,29 +3,23 @@ import { FormattedMessage } from 'react-intl';
 
 import { Headline } from '@folio/stripes-components';
 
-import AddContext from '../../AddContext';
 import Pluggable from '../../Pluggable';
-import { useStripes } from '../../StripesContext';
 
 import css from './NoPermissionScreen.css';
 
 const NoPermissionScreen = () => {
-  const stripes = useStripes();
-
   return (
-    <AddContext context={{ stripes }}>
-      <Pluggable type="frontpage">
-        <div className={css.titleWrap}>
-          <Headline
-            faded
-            tag="h2"
-            className={css.title}
-          >
-            <FormattedMessage id="stripes-core.front.error.noPermission" />
-          </Headline>
-        </div>
-      </Pluggable>
-    </AddContext>
+    <Pluggable type="frontpage">
+      <div className={css.titleWrap}>
+        <Headline
+          faded
+          tag="h2"
+          className={css.title}
+        >
+          <FormattedMessage id="stripes-core.front.error.noPermission" />
+        </Headline>
+      </div>
+    </Pluggable>
   );
 };
 
