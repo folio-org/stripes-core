72bfe6baa1d00aed347882098873e773b0142735
Zak Burke <zburke@ebsco.com>
2024-06-10 06:40:58 -0400

    provide default values so list.sort does not fail

--- src/components/About/AboutModules.js
+++ src/components/About/AboutModules.js
@@ -7,7 +7,7 @@ import {
 import css from './About.css';
 
 
-const AboutInterfaces = ({ list }) => {
+const AboutInterfaces = ({ list = [] }) => {
   list.sort((a, b) => a.name.localeCompare(b.name));
   return (
     <List
@@ -23,7 +23,7 @@ AboutInterfaces.propTypes = {
   list: PropTypes.arrayOf(PropTypes.object),
 };
 
-const AboutModules = ({ list }) => {
+const AboutModules = ({ list = [] }) => {
   list.sort((a, b) => a.name.localeCompare(b.name));
 
   return (
