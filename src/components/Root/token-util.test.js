070f7ca3ff3579a88d7d08600170d42cfe6503c2
Zak Burke <zburke@ebsco.com>
2024-10-21 15:12:07 -0400

    STCOR-895 wait a loooong time for a "stale" rotation request (#1548)
    As part of the RTR lifecycle, we write a rotation timestamp to local
    storage when the process starts and then remove it when it ends. This
    is a cheap way of making the rotation request visible across tabs,
    because all tabs read the same shared storage.
    
    To avoid the problem of a cancelled request leaving cruft in storage, we
    inspect that timestamp and consider a request "stale" if it's too old.
    That was the problem here: our "too old" timeout was too short; on a
    busy server, or on a slow connection, or on a client far from its host
    (say, in New Zealand), two seconds was not long enough. The rotation
    request would still be active when stripes considered it "stale",
    allowing a second request to go through. But since the first request
    was just slow, not dead, the second one is treated as a token-replay
    attack by the backend, causing all active sessions for that user account
    to be immediately terminated.
    
    Thus, waiting longer is a quick fix. A more detailed approach to
    tracking the request is detailed in the code-comments attached to #1547.
    
    Refs STCOR-895
    
    (cherry picked from commit b2083ccd3d176d9401a026073cac848ada6aff61)

--- src/components/Root/token-util.test.js
+++ src/components/Root/token-util.test.js
@@ -180,7 +180,7 @@ describe('rtr', () => {
 
       expect(ex).toBe(null);
       // expect(window.removeEventListener).toHaveBeenCalled();
-    });
+    }, 25000);
 
     it('multiple window (storage event)', async () => {
       const logger = {
@@ -214,7 +214,7 @@ describe('rtr', () => {
 
       expect(ex).toBe(null);
       // expect(window.removeEventListener).toHaveBeenCalledWith('monkey')
-    });
+    }, 25000);
   });
 
   it('on known error, throws error', async () => {
