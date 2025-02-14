folio-org/stripes-core

commit fa2ffcf45be74065fff73dff5b8af36e5f64725e

STCOR-942: migrate away from reading stripes-config::okapi.url (#1589)
Refs STCOR-942.

src/components/Login/LoginCtrl.js
@@ -18,6 +18,8 @@ class LoginCtrl extends Component {
   static propTypes = {
     authFailure: PropTypes.arrayOf(PropTypes.object),
     ssoEnabled: PropTypes.bool,
+    okapiUrl: PropTypes.string.isRequired,
+    tenant: PropTypes.string.isRequired,
     autoLogin: PropTypes.shape({
       username: PropTypes.string.isRequired,
       password: PropTypes.string.isRequired,
@@ -35,9 +37,6 @@ class LoginCtrl extends Component {
 
   constructor(props) {
     super(props);
-    this.sys = require('stripes-config'); // eslint-disable-line global-require
-    this.okapiUrl = this.sys.okapi.url;
-    this.tenant = this.sys.okapi.tenant;
     if (props.autoLogin && props.autoLogin.username) {
       this.handleSubmit(props.autoLogin);
     }
@@ -54,7 +53,7 @@ class LoginCtrl extends Component {
   }
 
   handleSubmit = (data) => {
-    return requestLogin(this.okapiUrl, this.context.store, this.tenant, data)
+    return requestLogin(this.props.okapiUrl, this.context.store, this.tenant, data)
       .then(this.handleSuccessfulLogin)
       .catch(e => {
         console.error(e); // eslint-disable-line no-console
@@ -62,7 +61,7 @@ class LoginCtrl extends Component {
   }
 
   handleSSOLogin = () => {
-    requestSSOLogin(this.okapiUrl, this.tenant);
+    requestSSOLogin(this.props.okapiUrl, this.props.tenant);
   }
 
   render() {
@@ -82,6 +81,8 @@ class LoginCtrl extends Component {
 const mapStateToProps = state => ({
   authFailure: state.okapi.authFailure,
   ssoEnabled: state.okapi.ssoEnabled,
+  okapiUrl: state.okapi.url,
+  tenant: state.okapi.tenant,
 });
 const mapDispatchToProps = dispatch => ({
   clearAuthErrors: () => dispatch(setAuthError([])),

src/components/Root/FFetch.js
@@ -42,7 +42,6 @@
  */
 
 import ms from 'ms';
-import { okapi as okapiConfig } from 'stripes-config';
 import {
   setRtrTimeout,
   setRtrFlsTimeout,
@@ -77,10 +76,11 @@ const OKAPI_FETCH_OPTIONS = {
 };
 
 export class FFetch {
-  constructor({ logger, store, rtrConfig }) {
+  constructor({ logger, store, rtrConfig, okapi }) {
     this.logger = logger;
     this.store = store;
     this.rtrConfig = rtrConfig;
+    this.okapi = okapi;
   }
 
   /**
@@ -232,12 +232,12 @@ export class FFetch {
    */
   ffetch = async (resource, options = {}) => {
     // FOLIO API requests are subject to RTR
-    if (isFolioApiRequest(resource, okapiConfig.url)) {
+    if (isFolioApiRequest(resource, this.okapi.url)) {
       this.logger.log('rtrv', 'will fetch', resource);
 
       // on authentication, grab the response to kick of the rotation cycle,
       // then return the response
-      if (isAuthenticationRequest(resource, okapiConfig.url)) {
+      if (isAuthenticationRequest(resource, this.okapi.url)) {
         this.logger.log('rtr', 'authn request', resource);
         return this.nativeFetch.apply(global, [resource, options && { ...options, ...OKAPI_FETCH_OPTIONS }])
           .then(res => {
@@ -266,7 +266,7 @@ export class FFetch {
       // tries to logout, the logout request will fail. And that's fine, just
       // fine. We will let them fail, capturing the response and swallowing it
       // to avoid getting stuck in an error loop.
-      if (isLogoutRequest(resource, okapiConfig.url)) {
+      if (isLogoutRequest(resource, this.okapi.url)) {
         this.logger.log('rtr', 'logout request');
 
         return this.nativeFetch.apply(global, [resource, options && { ...options, ...OKAPI_FETCH_OPTIONS }])

src/components/Root/FFetch.test.js
@@ -20,16 +20,6 @@ jest.mock('../../loginServices', () => ({
   getTokenExpiry: jest.fn(() => Promise.resolve())
 }));
 
-jest.mock('stripes-config', () => ({
-  url: 'okapiUrl',
-  tenant: 'okapiTenant',
-  okapi: {
-    url: 'okapiUrl',
-    tenant: 'okapiTenant'
-  }
-}),
-{ virtual: true });
-
 const log = jest.fn();
 
 const mockFetch = jest.fn();
@@ -50,7 +40,11 @@ describe('FFetch class', () => {
   describe('Calling a non-FOLIO API', () => {
     it('calls native fetch once', async () => {
       mockFetch.mockResolvedValueOnce('non-okapi-success');
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -63,7 +57,11 @@ describe('FFetch class', () => {
   describe('Calling a FOLIO API fetch', () => {
     it('calls native fetch once', async () => {
       mockFetch.mockResolvedValueOnce('okapi-success');
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
       const response = await global.fetch('okapiUrl/whatever', { testOption: 'test' });
@@ -91,6 +89,10 @@ describe('FFetch class', () => {
         logger: { log },
         store: {
           dispatch: jest.fn(),
+        },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
         }
       });
       testFfetch.replaceFetch();
@@ -110,7 +112,11 @@ describe('FFetch class', () => {
   describe('logging out', () => {
     it('calls native fetch once to log out', async () => {
       mockFetch.mockResolvedValueOnce('logged out');
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -124,7 +130,11 @@ describe('FFetch class', () => {
     it('fetch failure is silently trapped', async () => {
       mockFetch.mockRejectedValueOnce('logged out FAIL');
 
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -143,7 +153,11 @@ describe('FFetch class', () => {
   describe('logging out', () => {
     it('Calling an okapi fetch with valid token...', async () => {
       mockFetch.mockResolvedValueOnce('okapi success');
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -188,6 +202,10 @@ describe('FFetch class', () => {
         rtrConfig: {
           fixedLengthSessionWarningTTL: '1m',
         },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        }
       });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
@@ -244,6 +262,10 @@ describe('FFetch class', () => {
         rtrConfig: {
           fixedLengthSessionWarningTTL: '1m',
         },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        }
       });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
@@ -282,6 +304,10 @@ describe('FFetch class', () => {
         rtrConfig: {
           fixedLengthSessionWarningTTL: '1m',
         },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        }
       });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
@@ -317,6 +343,10 @@ describe('FFetch class', () => {
         logger: { log },
         store: {
           dispatch: jest.fn(),
+        },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
         }
       });
       testFfetch.replaceFetch();
@@ -361,6 +391,10 @@ describe('FFetch class', () => {
         rtrConfig: {
           fixedLengthSessionWarningTTL: '1m',
         },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        }
       });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
@@ -389,7 +423,11 @@ describe('FFetch class', () => {
     it('returns the error', async () => {
       mockFetch.mockResolvedValue('success')
         .mockResolvedValueOnce('failure');
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -411,7 +449,11 @@ describe('FFetch class', () => {
             },
           }
         ));
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -435,7 +477,11 @@ describe('FFetch class', () => {
           }
         ))
         .mockRejectedValueOnce(new Error('token error message'));
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -470,7 +516,11 @@ describe('FFetch class', () => {
             }
           }
         ));
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -500,7 +550,11 @@ describe('FFetch class', () => {
             }
           }
         ));
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 
@@ -530,7 +584,11 @@ describe('FFetch class', () => {
             }
           }
         ));
-      const testFfetch = new FFetch({ logger: { log } });
+      const testFfetch = new FFetch({ logger: { log },
+        okapi:{
+          url: 'okapiUrl',
+          tenant: 'okapiTenant'
+        } });
       testFfetch.replaceFetch();
       testFfetch.replaceXMLHttpRequest();
 

src/components/Root/FXHR.js
@@ -1,4 +1,3 @@
-import { okapi } from 'stripes-config';
 import { getPromise, isFolioApiRequest } from './token-util';
 import {
   RTR_ERROR_EVENT,
@@ -15,7 +14,7 @@ export default (deps) => {
 
     open = (method, url) => {
       this.FFetchContext.logger?.log('rtr', 'capture XHR.open');
-      this.shouldEnsureToken = isFolioApiRequest(url, okapi.url);
+      this.shouldEnsureToken = isFolioApiRequest(url, this.FFetchContext.okapi.url);
       super.open(method, url);
     }
 

src/components/Root/FXHR.test.js
@@ -24,7 +24,7 @@ describe('FXHR', () => {
   let testXHR;
   beforeEach(() => {
     jest.clearAllMocks();
-    FakeXHR = FXHR({ tokenExpiration: { atExpires: Date.now(), rtExpires: Date.now() + 5000 }, logger: { log: () => {} } });
+    FakeXHR = FXHR({ tokenExpiration: { atExpires: Date.now(), rtExpires: Date.now() + 5000 }, logger: { log: () => {} }, okapi:{ url:'okapiUrl' } });
     testXHR = new FakeXHR();
   });
 

src/components/Root/Root.js
@@ -73,6 +73,7 @@ class Root extends Component {
         logger: this.props.logger,
         store,
         rtrConfig,
+        okapi
       });
       this.ffetch.replaceFetch();
       this.ffetch.replaceXMLHttpRequest();

src/components/SSOLanding/useSSOSession.js
@@ -4,7 +4,7 @@ import { useLocation } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 import queryString from 'query-string';
 
-import { config, okapi } from 'stripes-config';
+import { config } from 'stripes-config';
 
 import { defaultErrors } from '../../constants';
 import { setAuthError } from '../../okapiActions';
@@ -45,7 +45,7 @@ const useSSOSession = () => {
   const tenant = getTenant(params, token, store);
 
   useEffect(() => {
-    requestUserWithPerms(okapi.url, store, tenant, token)
+    requestUserWithPerms(store.getState().okapi.url, store, tenant, token)
       .then(() => {
         if (store.getState()?.okapi?.authFailure) {
           return Promise.reject(new Error('SSO Failed'));

src/components/SSOLanding/useSSOSession.test.js
@@ -4,7 +4,7 @@ import { useDispatch, useStore } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 
-import { config, okapi } from 'stripes-config';
+import { config } from 'stripes-config';
 
 import { defaultErrors } from '../../constants';
 import { setAuthError } from '../../okapiActions';
@@ -30,12 +30,10 @@ jest.mock('stripes-config', () => ({
   config: {
     useSecureTokens: true,
   },
-  okapi: {
-    url: 'okapiUrl',
-    tenant: 'okapiTenant'
-  }
 }),
 { virtual: true });
+
+jest.mock('');
 
 jest.mock('../../loginServices', () => ({
   requestUserWithPerms: jest.fn()
@@ -75,7 +73,7 @@ describe('SSOLanding', () => {
 
     renderHook(() => useSSOSession());
 
-    expect(requestUserWithPerms).toHaveBeenCalledWith(okapi.url, store, okapi.tenant, ssoTokenValue);
+    expect(requestUserWithPerms).toHaveBeenCalledWith(store.getState().okapi.url, store, store.getState().okapi.tenant, ssoTokenValue);
   });
 
   it('should request user session when RTR is disabled with token from cookies', () => {
@@ -86,7 +84,7 @@ describe('SSOLanding', () => {
 
     renderHook(() => useSSOSession());
 
-    expect(requestUserWithPerms).toHaveBeenCalledWith(okapi.url, store, okapi.tenant, ssoTokenValue);
+    expect(requestUserWithPerms).toHaveBeenCalledWith(store.getState().okapi.url, store, 'okapiTenant', ssoTokenValue);
   });
 
   it('should request user session when RTR is disabled and right tenant from ssoToken', () => {
@@ -99,7 +97,7 @@ describe('SSOLanding', () => {
 
     renderHook(() => useSSOSession());
 
-    expect(requestUserWithPerms).toHaveBeenCalledWith(okapi.url, store, tokenTenant, ssoTokenValue);
+    expect(requestUserWithPerms).toHaveBeenCalledWith(store.getState().okapi.url, store, tokenTenant, ssoTokenValue);
   });
 
   it('should request user session when RTR is enabled and right tenant from query params', () => {
@@ -111,7 +109,7 @@ describe('SSOLanding', () => {
 
     renderHook(() => useSSOSession());
 
-    expect(requestUserWithPerms).toHaveBeenCalledWith(okapi.url, store, queryTenant, undefined);
+    expect(requestUserWithPerms).toHaveBeenCalledWith(store.getState().okapi.url, store, queryTenant, undefined);
   });
 
   it('should display error when session request failed', async () => {

commit 92f93e9f8cb6399cf0d44629b6cb7321c82edc56

STCOR-942 properly retrieve tenant from state
Fetch `tenant` from state instead of from `stripes-config`. This
resolves a bug introduced in #1589 that left a dangling reference to
`this.tenant` that was no longer populated.

Refs STCOR-942

src/components/Login/LoginCtrl.js
@@ -37,9 +37,6 @@ class LoginCtrl extends Component {
 
   constructor(props) {
     super(props);
-    this.sys = require('stripes-config'); // eslint-disable-line global-require
-    this.okapiUrl = this.sys.okapi.url;
-    this.tenant = this.sys.okapi.tenant;
     if (props.autoLogin && props.autoLogin.username) {
       this.handleSubmit(props.autoLogin);
     }
@@ -56,7 +53,7 @@ class LoginCtrl extends Component {
   }
 
   handleSubmit = (data) => {
-    return requestLogin(this.props.okapiUrl, this.context.store, this.tenant, data)
+    return requestLogin(this.props.okapiUrl, this.context.store, this.props.tenant, data)
       .then(this.handleSuccessfulLogin)
       .catch(e => {
         console.error(e); // eslint-disable-line no-console
@@ -64,7 +61,7 @@ class LoginCtrl extends Component {
   }
 
   handleSSOLogin = () => {
-    requestSSOLogin(this.props.okapiUrl, this.tenant);
+    requestSSOLogin(this.props.okapiUrl, this.props.tenant);
   }
 
   render() {