2c9c22fc0cc2682bd8df1580a6624a6bb8dbfe3a
John Coburn <jcoburn@ebsco.com>
2025-04-07 16:03:46 -0500

    filter out formatjs gripes in console

--- test/bigtest/helpers/setup-application.js
+++ test/bigtest/helpers/setup-application.js
@@ -113,6 +113,13 @@ export default function setupApplication({
       visit: { value: visit },
       location: { get: location },
     });
+
+    const origWarn = console.warn;
+    console.warn = (msg) => {
+      if (!msg.includes('[@formatjs/intl]')) {
+        origWarn(msg);
+      }
+    };
   });
 
   afterEach(async () => {
