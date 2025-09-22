109742a12de54659c1ca9c5e04588aa6a0ee4237
John Coburn <jcoburn@ebsco.com>
2025-09-22 11:14:56 -0500

    STCOR-941 Convert test suites to Newschool BT (#1609)
    Much of this was accomplished back when we jettisoned bigtest-mocha.
    
    This PR cleans up the remainder of test suites still making use of
    old-school interactors. The interactors ARE still around to be ickily
    imported, but can be flat out removed w/o breaking _this_ test suite. As
    we tread forward, we'll eventually get around to removing all `@bigtest`
    deps and moving necessary interactors (if needed) to
    `@folio/stripes-testing`. This PR also restores some tests that were
    previously skipped.
    
    Refs STCOR-941

--- test/bigtest/interactors/useCustomFields.js
+++ test/bigtest/interactors/useCustomFields.js
@@ -15,3 +15,19 @@ import { clickable, fillable, interactor, isVisible, text } from '@bigtest/inter
 }
 
 export default UseCustomFieldsInteractor;
+
+const UseCustomFieldsInteractor = HTML.extend('UseCustomField')
+  .selector('body')
+  .filters({
+    errorShown: el => Boolean(el.querySelector('#error')),
+    loading: el => Boolean(el.querySelector('#loading')),
+    customFields: el => Boolean(el.querySelector('#custom-fields li')),
+    customFields: el =>  el.querySelector('#custom-fields')?.innerText,
+  })
+  .actions ({
+    fillUserName: ({find}, value) => { find(TextField({ id: 'input-username' }).fill(value))},
+    fillPassword: ({find}, value) => { find(TextField({ id: 'input-password' }).fill(value))},
+    clickLogin: ({find}) => {find(Button({ id: 'clickable-login' })).click()},
+    clickProfileDropdown: ({find}) => {find(Button({ id: 'profileDropdown' })).click()},
+    clickLogout: ({find}) => {find(Button({ id: 'clickable-logout' })).click()},
+  });
