2ec5c2b19ed2369066e9c52430f27b64d3c7ff82
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-10-08 14:49:44 +0100

    docs: Tweak comment on useChunkedCQLFetch hook

--- src/queries/useChunkedCQLFetch.js
+++ src/queries/useChunkedCQLFetch.js
@@ -1,8 +1,11 @@
 import useChunkedIdTransformFetch from './useChunkedIdTransformFetch';
 
-// useChunkedCQLFetch remains to avoid backwards compatibility issues,
-// but makes use of generalised useChunkedIdTransformFetch
-// This is the place to make changes and additions which can be consumed by CQL type API endpoints
+// When fetching from a potentially large list of items, split the list over
+// multiple requests and then reassemble the results in order to avoid
+// a HTTP 414/URI Too Long error.
+
+// This hook handles CQL endpoints only; see useChunkedIdTransformFetch
+// for a generic implementation.
 const useChunkedCQLFetch = ({
   idName = 'id', // Named ID field to use in the CQL query (i.e. id or userId)
   limit = 1000, // Item limit to fetch on each request
