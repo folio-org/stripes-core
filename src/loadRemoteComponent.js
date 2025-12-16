779b6ecc5587cd21e115be5b711aca21138c6809
John Coburn <jcoburn@ebsco.com>
2025-12-16 15:10:45 -0600

    add error handling to loadRemoteComponent, update loadRemoteComponent tests

--- src/loadRemoteComponent.js
+++ src/loadRemoteComponent.js
@@ -1,23 +1,32 @@
 // https://webpack.js.org/concepts/module-federation/#dynamic-remote-containers
 export default async function loadRemoteComponent(remoteUrl, remoteName) {
-  if (!window[remoteName]) {
-    const response = await fetch(remoteUrl);
-    const source = await response.text();
-    const script = document.createElement('script');
-    script.textContent = source;
-    document.body.appendChild(script);
+  const errorMessage = `Failed to fetch remote module from ${remoteUrl}`;
+  try {
+    if (!window[remoteName]) {
+      const response = await fetch(remoteUrl);
+      if (!response.ok) {
+        throw new Error(errorMessage);
+      }
+      const source = await response.text();
+      const script = document.createElement('script');
+      script.textContent = source;
+      document.body.appendChild(script);
+    }
+
+    const container = window[remoteName];
+
+    // eslint-disable-next-line no-undef
+    await __webpack_init_sharing__('default');
+
+    // eslint-disable-next-line no-undef
+    await container.init(__webpack_share_scopes__.default);
+
+    const factory = await container.get('./MainEntry');
+    const Module = await factory();
+
+    return Module;
+  } catch (error) {
+    console.error(`${errorMessage}`, error);
+    throw error;
   }
-
-  const container = window[remoteName];
-
-  // eslint-disable-next-line no-undef
-  await __webpack_init_sharing__('default');
-
-  // eslint-disable-next-line no-undef
-  await container.init(__webpack_share_scopes__.default);
-
-  const factory = await container.get('./MainEntry');
-  const Module = await factory();
-
-  return Module;
 }
--- test/bigtest/tests/LoadRemoteComponent-test.js
+++ test/bigtest/tests/LoadRemoteComponent-test.js
@@ -1,17 +1,18 @@
-import { beforeEach, it, describe } from 'mocha';
+import { beforeEach, it, afterEach, describe } from 'mocha';
 import { expect } from 'chai';
-import setupApplication from '../helpers/setup-application-components';
+import startMirage from '../network/start';
 import loadRemoteComponent from '../../../src/loadRemoteComponent';
 
-describe('loadRemoteComponent', () => {
-  setupApplication();
+describe.only('loadRemoteComponent', () => {
+  let server;
   const mockRemoteUrl = 'http://example.com/testRemote/remoteEntry.js';
   const mockErrorUrl = 'http://example.com/nonexistent/remoteEntry.js';
 
   const mockRemoteName = 'testComponent';
 
   beforeEach(async function () {
-    this.server.get(mockRemoteUrl, () => {
+    server = startMirage();
+    server.get(mockRemoteUrl, () => {
       const mockScriptContent = `window['${mockRemoteName}'] = {
         init: function() { console.log("Component initialized"); },
         get: function() { return function() { return { default: 'I am a module' }; }}
@@ -21,7 +22,13 @@ describe('loadRemoteComponent', () => {
       return mockScriptContent;
     });
 
-    this.server.get(mockErrorUrl, () => new Response(404));
+    server.get(mockErrorUrl, () => (server.serialize({ ok: false })));
+  });
+
+  afterEach(function () {
+    server?.shutdown();
+    server = null;
+    delete window[mockRemoteName];
   });
 
   it('should load and evaluate the remote script', async () => {
@@ -29,11 +36,11 @@ describe('loadRemoteComponent', () => {
     expect(window[mockRemoteName]).to.be.an('object');
   });
 
-  it('should return the component from the remote script', async () => {
+  it('should handle errors when loading the remote script', async () => {
     try {
       await loadRemoteComponent(mockErrorUrl, mockRemoteName);
     } catch (error) {
-      expect(error.message).to.equal('Failed to load remote component');
+      expect(error.message).to.equal(`Failed to fetch remote module from ${mockErrorUrl}`);
     }
   });
 });
