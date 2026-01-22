d9c47d897368a5f96d6158baa90f70adb5af4254
John Coburn <jcoburn@ebsco.com>
2026-01-22 09:43:43 -0600

    add a constant for the name of stripes in the host app bundle, shape discovery results/pull discovery results from localForage

--- src/components/loadEntitlement.js
+++ src/components/loadEntitlement.js
@@ -1,12 +1,23 @@
-export const loadEntitlement = async (entitlementUrl) => {
-  const res = await fetch(entitlementUrl);
-  const registry = await res.json();
+import localforage from 'localforage';
+import { stripesHubAPI } from '../constants';
 
-  // process the registry data and return the remotes array
-  // remap registry from an object shaped like { key1: app1, key2: app2, ...}
-  // to an array shaped like [ { name: key1, ...app1 }, { name: key2, ...app2 } ...]
-  // const remotes = Object.entries(registry?.discovery).map(([name, metadata]) => ({ name, ...metadata }));
-  // split location into host, port for asset loading.
+export const loadEntitlement = async (entitlementUrl) => {
+  let registry;
+  const discovery = await localforage.getItem(stripesHubAPI.REMOTE_LIST_KEY);
+  if (discovery) {
+    registry = { discovery };
+  } else {
+    const res = await fetch(entitlementUrl);
+    const registryData = await res.json();
+
+    // strip out the host app if it's present...
+    registry.discovery = registryData.filter((entry) => entry.name !== stripesHubAPI.HOST_APP_NAME);
+  }
+
+  // Take the location information for each remote in the response and split out its origin...
+  // i.e. 'http://localhost:3002/remoteEntry.js -> 'http://localhost:3002'
+  // this origin is where stripes-core will attempt to fetch translations and assets from.
+
   registry.discovery.forEach(remote => {
     if (!remote?.location?.startsWith('http')) {
       remote.location = `${window.location.protocol}//${remote.location}`;
--- src/constants/stripesHubAPI.js
+++ src/constants/stripesHubAPI.js
@@ -4,5 +4,6 @@
 export default {
   HOST_LOCATION_KEY: 'hostLocation',
   REMOTE_LIST_KEY: 'entitlements',
-  ENTITLEMENT_URL_KEY: 'entitlementUrl'
+  ENTITLEMENT_URL_KEY: 'entitlementUrl',
+  HOST_APP_NAME: 'folio_stripes',
 };
