4ae4e44e992ff7a4769d253836b729bc2e32a2cd
John Coburn <jcoburn@ebsco.com>
2026-03-20 14:18:24 -0500

    use updated stripesHubAPI constant

--- src/constants/stripesHubAPI.js
+++ src/constants/stripesHubAPI.js
@@ -2,7 +2,7 @@
 // These function to allow a remotely loaded host app to
 // load translations and refresh the list of entitled modules.
 export default {
-  HOST_LOCATION_KEY: 'hostLocation',
+  HOST_URL_KEY: 'hostUrl',
   REMOTE_LIST_KEY: 'entitlements',
   DISCOVERY_URL_KEY: 'discoveryUrl',
   HOST_APP_NAME: 'folio_stripes',
